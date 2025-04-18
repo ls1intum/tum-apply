8751c3e81878886e6a50f5303edc7b1c05dac861
Sarah Douglas <sarah.douglas@gmx.de>
2025-04-18 15:21:52 +0200

    adjustment: adjusted reject reason (will not be saved in database)

---
+++ src/main/java/de/tum/cit/aet/application/domain/RejectReason.java
@@ -0,0 +1,25 @@
+package de.tum.cit.aet.application.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * The client will ask the server for a list of possible reject reasons.
+ * The supervisor can use and adapt those to their needs.
+ * This method is chosen because lateron instead of an enum a database can be used
+ * and each chair can implement custom Reject Reason Texts.
+ */
+
+@Getter
+@AllArgsConstructor
+public enum RejectReason {
+    FAILED_APPLICANT_REQUIREMENTS("FAILED_APPLICANT_REQUIREMENTS"),
+    POSITION_ALREADY_FILLED("POSITION_ALREADY_FILLED"),
+    NOT_SUFFICIENT_GRADES("NOT_SUFFICIENT_GRADES");
+
+    private String value;
+
+    public String getStandardText(String languageKey) {
+        return "Not yet implemented";
+    }
+}
