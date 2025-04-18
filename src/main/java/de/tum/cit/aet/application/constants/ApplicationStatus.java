58428dd0c931c1d6d53dd9118f0355cb048fbb6c
Sarah Douglas <sarah.douglas@gmx.de>
2025-04-18 15:31:07 +0200

    adjustment: more application statuses

--- src/main/java/de/tum/cit/aet/application/constants/ApplicationStatus.java
+++ src/main/java/de/tum/cit/aet/application/constants/ApplicationStatus.java
@@ -6,9 +6,26 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ApplicationStatus {
-    ACCEPTED("ACCEPTED"),
-    IN_REVIEW("IN_REVIEW"),
-    REJECTED("REJECTED");
+    SAVED("SAVED", ApplicationStatusIssuer.APPLICANT),
+    SENT("SENT", ApplicationStatusIssuer.APPLICANT),
+    ACCEPTED("ACCEPTED", ApplicationStatusIssuer.SUPERVISOR),
+    IN_REVIEW("IN_REVIEW", ApplicationStatusIssuer.SUPERVISOR),
+    REJECTED("REJECTED", ApplicationStatusIssuer.SUPERVISOR),
+    WITHDRAWN("WITHDRAWN", ApplicationStatusIssuer.APPLICANT);
 
     private final String value;
+    private final ApplicationStatusIssuer applicationStatusIssuer;
+}
+
+@Getter
+@AllArgsConstructor
+enum ApplicationStatusIssuer {
+    SUPERVISOR("SUPERVISOR"),
+    APPLICANT("APPLICANT")
+    /* TODO when we introduce admin abilities, 
+       it would make sense to have admin induced Applicationstatuses like FLAGGED (if someone misbehaves) or ARCHIVED f.e. */
+    // ADMIN("ADMIN")
+    ;
+
+    private String value;
 }
