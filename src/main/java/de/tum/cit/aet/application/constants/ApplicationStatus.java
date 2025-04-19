b3d992790f779aaf51e4b3113969eada56f5e98d
Sarah Douglas <sarah.douglas@gmx.de>
2025-04-19 09:56:42 +0200

    fix: replaced unnecessary enum with already existing Usergroup enum

--- src/main/java/de/tum/cit/aet/application/constants/ApplicationStatus.java
+++ src/main/java/de/tum/cit/aet/application/constants/ApplicationStatus.java
@@ -1,31 +1,27 @@
 package de.tum.cit.aet.application.constants;
 
+import de.tum.cit.aet.usermanagement.constants.UserGroup;
+import java.util.Arrays;
+import java.util.EnumSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public enum ApplicationStatus {
-    SAVED("SAVED", ApplicationStatusIssuer.APPLICANT),
-    SENT("SENT", ApplicationStatusIssuer.APPLICANT),
-    ACCEPTED("ACCEPTED", ApplicationStatusIssuer.SUPERVISOR),
-    IN_REVIEW("IN_REVIEW", ApplicationStatusIssuer.SUPERVISOR),
-    REJECTED("REJECTED", ApplicationStatusIssuer.SUPERVISOR),
-    WITHDRAWN("WITHDRAWN", ApplicationStatusIssuer.APPLICANT);
+    SAVED("SAVED", EnumSet.of(UserGroup.APPLICANT)),
+    SENT("SENT", EnumSet.of(UserGroup.APPLICANT)),
+    ACCEPTED("ACCEPTED", EnumSet.of(UserGroup.RESEARCH_ASSISTANT, UserGroup.PROFESSOR)),
+    IN_REVIEW("IN_REVIEW", EnumSet.of(UserGroup.RESEARCH_ASSISTANT, UserGroup.PROFESSOR)),
+    REJECTED("REJECTED", EnumSet.of(UserGroup.RESEARCH_ASSISTANT, UserGroup.PROFESSOR)),
+    WITHDRAWN("WITHDRAWN", EnumSet.of(UserGroup.APPLICANT));
 
     private final String value;
-    private final ApplicationStatusIssuer applicationStatusIssuer;
-}
+    private final Set<UserGroup> userGroups;
 
-@Getter
-@AllArgsConstructor
-enum ApplicationStatusIssuer {
-    SUPERVISOR("SUPERVISOR"),
-    APPLICANT("APPLICANT")
-    /* TODO when we introduce admin abilities, 
-       it would make sense to have admin induced Applicationstatuses like FLAGGED (if someone misbehaves) or ARCHIVED f.e. */
-    // ADMIN("ADMIN")
-    ;
-
-    private String value;
+    public static Set<ApplicationStatus> getByUserGroup(UserGroup group) {
+        return Arrays.stream(values()).filter(status -> status.getUserGroups().contains(group)).collect(Collectors.toSet());
+    }
 }
