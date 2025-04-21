8a37e226a67b33210f2e8d1c51218df5f6b5a4a4
Sarah Douglas <sarah.douglas@gmx.de>
2025-04-21 12:58:08 +0200

    `Development`: Add more application statuses (#65)
    * adjustment: more application statuses
    
    * fix: replaced unnecessary enum with already existing Usergroup enum
    
    * fix: adjusted method name

--- src/main/java/de/tum/cit/aet/application/constants/ApplicationStatus.java
+++ src/main/java/de/tum/cit/aet/application/constants/ApplicationStatus.java
@@ -1,14 +1,27 @@
 package de.tum.cit.aet.application.constants;
 
+import de.tum.cit.aet.usermanagement.constants.UserGroup;
+import java.util.Arrays;
+import java.util.EnumSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public enum ApplicationStatus {
-    ACCEPTED("ACCEPTED"),
-    IN_REVIEW("IN_REVIEW"),
-    REJECTED("REJECTED");
+    SAVED("SAVED", EnumSet.of(UserGroup.APPLICANT)),
+    SENT("SENT", EnumSet.of(UserGroup.APPLICANT)),
+    ACCEPTED("ACCEPTED", EnumSet.of(UserGroup.RESEARCH_ASSISTANT, UserGroup.PROFESSOR)),
+    IN_REVIEW("IN_REVIEW", EnumSet.of(UserGroup.RESEARCH_ASSISTANT, UserGroup.PROFESSOR)),
+    REJECTED("REJECTED", EnumSet.of(UserGroup.RESEARCH_ASSISTANT, UserGroup.PROFESSOR)),
+    WITHDRAWN("WITHDRAWN", EnumSet.of(UserGroup.APPLICANT));
 
     private final String value;
+    private final Set<UserGroup> userGroups;
+
+    public static Set<ApplicationStatus> getPermittedStatusesByUserGroup(UserGroup group) {
+        return Arrays.stream(values()).filter(status -> status.getUserGroups().contains(group)).collect(Collectors.toSet());
+    }
 }
