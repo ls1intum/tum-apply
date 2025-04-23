a4d8308a7532d18756c3bcfc43eed6c606cfdd8c
Sarah Douglas <sarah.douglas@gmx.de>
2025-04-23 16:26:15 +0200

    fix: addendum, forgot to commit

--- src/main/java/de/tum/cit/aet/application/repository/ApplicationReviewRepository.java
+++ src/main/java/de/tum/cit/aet/application/repository/ApplicationReviewRepository.java
@@ -1,6 +1,6 @@
 package de.tum.cit.aet.application.repository;
 
-import de.tum.cit.aet.application.domain.ApplicationReview;
+import de.tum.cit.aet.evaluation.domain.ApplicationReview;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
