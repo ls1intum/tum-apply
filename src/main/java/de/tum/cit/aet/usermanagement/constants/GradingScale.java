d5f38756ff3ec53a86197cb39cbc742fc946d00c
Ishani Budhwar <ge87tev@mytum.de>
2025-04-26 19:14:16 +0200

    fix: Only support one grading scale in the beginning.

--- src/main/java/de/tum/cit/aet/usermanagement/constants/GradingScale.java
+++ src/main/java/de/tum/cit/aet/usermanagement/constants/GradingScale.java
@@ -6,11 +6,15 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Getter
 public enum GradingScale {
+    ONE_TO_FOUR("1.0 best – 4.0 worst (e.g., Germany)");
+
+    // Future Expansion: Add more grading scales as needed
+    /*
     FOUR_POINT("4.0 best – 0.0 worst (e.g., USA, Canada)"),
-    ONE_TO_FOUR("1.0 best – 4.0 worst (e.g., Germany)"),
     LETTER("A best – F worst (e.g., Europe)"),
     PERCENTAGE("100% best – 0% worst (e.g., UK, China, others)"),
     TEN_POINT("10 best – 0 worst (e.g., India, Brazil)");
+     */
 
     private final String description;
 }
