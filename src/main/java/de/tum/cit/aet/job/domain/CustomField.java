b30eb39e795eeb512c870aff03ee9f2fed7639b2
Ishani Budhwar <ge87tev@mytum.de>
2025-04-28 10:28:15 +0200

    fix: Make required columns in CustomField Entity non-nullable

--- src/main/java/de/tum/cit/aet/job/domain/CustomField.java
+++ src/main/java/de/tum/cit/aet/job/domain/CustomField.java
@@ -22,22 +22,23 @@ public class CustomField {
     private UUID customFieldId;
 
     @ManyToOne
-    @JoinColumn(name = "job_id")
+    @JoinColumn(name = "job_id", nullable = false)
     private Job job;
 
-    private int sequence;
+    @Column(name = "question", nullable = false)
+    private String question;
 
-    @Column(name = "custom_field_type")
-    private CustomFieldType customFieldType;
-
-    @Column(name = "is_required")
+    @Column(name = "is_required", nullable = false)
     private boolean isRequired;
 
-    private String question;
+    @Column(name = "custom_field_type", nullable = false)
+    private CustomFieldType customFieldType;
 
     @Convert(converter = StringListConverter.class)
     @Column(name = "answer_options")
     private List<String> answerOptions;
+
+    private int sequence;
 
     @OneToMany(mappedBy = "customField", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<CustomFieldAnswer> customFieldAnswers;
