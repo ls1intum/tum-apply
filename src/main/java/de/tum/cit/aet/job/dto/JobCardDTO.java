17efd3171bbab02016a1fdcd30054d83e8875061
aniruddhzaveri <aniruddh.zaveri@tum.de>
2025-05-01 15:27:26 +0200

    open api fixed

--- src/main/java/de/tum/cit/aet/job/dto/JobCardDTO.java
+++ src/main/java/de/tum/cit/aet/job/dto/JobCardDTO.java
@@ -2,19 +2,20 @@ package de.tum.cit.aet.job.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import de.tum.cit.aet.job.constants.JobState;
+import jakarta.validation.constraints.NotNull;
 import java.time.Instant;
 import java.util.UUID;
 
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public record JobCardDTO(
-    UUID id,
-    String title,
-    String fieldOfStudies,
-    String location,
-    UUID professorId,
-    int workload,
-    Instant startDate,
-    String description,
-    JobState state,
-    Instant createdAt
+    @NotNull UUID id,
+    @NotNull String title,
+    @NotNull String fieldOfStudies,
+    @NotNull String location,
+    @NotNull UUID professorId,
+    @NotNull int workload,
+    @NotNull Instant startDate,
+    @NotNull String description,
+    @NotNull JobState state,
+    @NotNull Instant createdAt
 ) {}
