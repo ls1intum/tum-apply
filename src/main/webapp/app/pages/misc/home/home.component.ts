8ef488b0b8adce4f97cdaa7bf083b2421410b455
Sarah Douglas <sarah.douglas@gmx.de>
2025-05-05 12:52:10 +0200

    fix: standalone components over modules

--- src/main/webapp/app/pages/misc/home/home.component.ts
+++ src/main/webapp/app/pages/misc/home/home.component.ts
@@ -4,13 +4,13 @@ import SharedModule from 'app/shared/shared.module';
 import { LoginService } from 'app/pages/usermanagement/login/login.service';
 import { AccountService } from 'app/core/auth/account.service';
 import { Account } from 'app/core/auth/account.model';
-import { AtomsModule } from 'app/shared/components/atoms/atoms.module';
+import { ButtonComponent } from 'app/shared/components/atoms/button/button.component';
 
 @Component({
   selector: 'jhi-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
-  imports: [SharedModule, RouterModule, AtomsModule],
+  imports: [SharedModule, RouterModule, ButtonComponent],
 })
 export default class HomeComponent implements OnInit {
   account = signal<Account | null>(null);
--- src/main/webapp/app/shared/components/atoms/button/button.component.ts
+++ src/main/webapp/app/shared/components/atoms/button/button.component.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'jhi-button',
   templateUrl: './button.component.html',
   styleUrl: './button.component.scss',
-  standalone: false,
+  standalone: true,
+  imports: [CommonModule, ButtonModule],
 })
 export class ButtonComponent {}
