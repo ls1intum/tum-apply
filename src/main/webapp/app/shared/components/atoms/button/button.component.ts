2697efb34c61c3258317c300e48fcd8bc70bdb90
Sarah Douglas <sarah.douglas@gmx.de>
2025-05-05 14:56:09 +0200

    `Development`: Setup primeng, bootstrap dependencies (#111)
    * fix: Setup primeng, bootstrap dependencies
    
    * fix: standalone components over modules
    
    * fix: eslint

--- src/main/webapp/app/shared/components/atoms/button/button.component.ts
+++ src/main/webapp/app/shared/components/atoms/button/button.component.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'jhi-button',
-  imports: [],
   templateUrl: './button.component.html',
   styleUrl: './button.component.scss',
+  standalone: true,
+  imports: [CommonModule, ButtonModule],
 })
 export class ButtonComponent {}
