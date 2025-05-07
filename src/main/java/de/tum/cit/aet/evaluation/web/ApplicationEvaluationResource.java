9acf6e53ddc412f138946c79622cc4df468e49e9
Moritz <moritzschmidt1@gmail.com>
2025-05-07 14:14:01 +0200

    validation of Pagination Parameters

--- src/main/java/de/tum/cit/aet/evaluation/web/ApplicationEvaluationResource.java
+++ src/main/java/de/tum/cit/aet/evaluation/web/ApplicationEvaluationResource.java
@@ -3,9 +3,11 @@ package de.tum.cit.aet.evaluation.web;
 import de.tum.cit.aet.evaluation.dto.ApplicationEvaluationListDTO;
 import de.tum.cit.aet.evaluation.service.ApplicationEvaluationService;
 import de.tum.cit.aet.usermanagement.domain.ResearchGroup;
+import jakarta.validation.constraints.Min;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/evaluation")
 @AllArgsConstructor
+@Validated
 public class ApplicationEvaluationResource {
 
     private final ApplicationEvaluationService applicationEvaluationService;
@@ -28,8 +31,8 @@ public class ApplicationEvaluationResource {
      */
     @GetMapping("/applications")
     public ResponseEntity<ApplicationEvaluationListDTO> getApplications(
-        @RequestParam(required = false, defaultValue = "25") int pageSize,
-        @RequestParam(required = false, defaultValue = "0") int pageNumber
+        @RequestParam(required = false, defaultValue = "25") @Min(1) int pageSize,
+        @RequestParam(required = false, defaultValue = "0") @Min(0) int pageNumber
     ) {
         //TODO this will be removed when the ResearchGroup can be accessed through the authenticated user
         ResearchGroup researchGroup = new ResearchGroup();
