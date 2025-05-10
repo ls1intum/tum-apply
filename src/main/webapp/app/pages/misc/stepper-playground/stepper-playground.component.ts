3e93b04af546867d59b9edd5556e80994a1664bd
Sarah Douglas <sarah.douglas@gmx.de>
2025-05-10 23:59:56 +0200

    feat: stepper component

---
+++ src/main/webapp/app/pages/misc/stepper-playground/stepper-playground.component.ts
@@ -0,0 +1,116 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { ProgressStepperComponent, StepData } from 'app/shared/components/molecules/progress-stepper/progress-stepper.component';
+
+@Component({
+  selector: 'jhi-stepper-playground',
+  imports: [CommonModule, ProgressStepperComponent],
+  templateUrl: './stepper-playground.component.html',
+  styleUrl: './stepper-playground.component.scss',
+  standalone: true,
+})
+export class StepperPlaygroundComponent implements OnInit {
+  @ViewChild('panel1', { static: true }) panel1!: TemplateRef<any>;
+  @ViewChild('panel2', { static: true }) panel2!: TemplateRef<any>;
+  @ViewChild('panel3', { static: true }) panel3!: TemplateRef<any>;
+
+  data: StepData[] = [];
+
+  ngOnInit() {
+    this.data = [
+      {
+        name: 'panel1 name',
+        panelTemplate: this.panel1,
+        buttonGroupPrev: [],
+        buttonGroupNext: [
+          {
+            variant: 'filled',
+            color: 'secondary',
+            icon: 'save',
+            onClick: () => {},
+            disabled: false,
+            label: 'Save Draft',
+          },
+          {
+            variant: 'filled',
+            color: 'primary',
+            icon: 'arrow-right',
+            onClick: () => {
+              alert('Clicked');
+            },
+            disabled: false,
+            label: 'Next',
+            changePanel: true,
+          },
+        ],
+      },
+      {
+        name: 'panel2 name',
+        panelTemplate: this.panel2,
+        buttonGroupPrev: [
+          {
+            variant: 'outlined',
+            color: 'primary',
+            icon: 'arrow-left',
+            onClick: () => {
+              alert('Clicked');
+            },
+            disabled: false,
+            label: 'Prev',
+            changePanel: true,
+          },
+        ],
+        buttonGroupNext: [
+          {
+            variant: 'filled',
+            color: 'secondary',
+            icon: 'save',
+            onClick: () => {},
+            disabled: false,
+            label: 'Save Draft',
+          },
+          {
+            variant: 'filled',
+            color: 'primary',
+            icon: 'arrow-right',
+            onClick: () => {
+              alert('Clicked');
+            },
+            disabled: false,
+            label: 'Next',
+            changePanel: true,
+          },
+        ],
+      },
+      {
+        name: 'panel3 name',
+        panelTemplate: this.panel3,
+        buttonGroupPrev: [
+          {
+            variant: 'outlined',
+            color: 'primary',
+            icon: 'arrow-left',
+            onClick: () => {
+              alert('Clicked');
+            },
+            disabled: false,
+            label: 'Prev',
+            changePanel: true,
+          },
+        ],
+        buttonGroupNext: [
+          {
+            variant: 'filled',
+            color: 'primary',
+            icon: 'paper-plane',
+            onClick: () => {
+              alert('Clicked');
+            },
+            disabled: false,
+            label: 'Send',
+          },
+        ],
+      },
+    ];
+  }
+}
---
+++ src/main/webapp/app/shared/components/molecules/progress-stepper/progress-stepper.component.ts
@@ -0,0 +1,54 @@
+import { Component, input, TemplateRef } from '@angular/core';
+import { Button } from '../../atoms/button/button.component';
+import { CommonModule } from '@angular/common';
+import { StepperModule } from 'primeng/stepper';
+import ButtonGroupComponent, { ButtonGroupData } from '../button-group/button-group.component';
+
+export type StepButton = Button & {
+  changePanel?: boolean;
+};
+
+export type StepData = {
+  name: string;
+  panelTemplate: TemplateRef<any>;
+  buttonGroupPrev: StepButton[];
+  buttonGroupNext: StepButton[];
+};
+
+@Component({
+  selector: 'jhi-progress-stepper',
+  imports: [CommonModule, StepperModule, ButtonGroupComponent],
+  templateUrl: './progress-stepper.component.html',
+  styleUrl: './progress-stepper.component.scss',
+  standalone: true,
+})
+export class ProgressStepperComponent {
+  currentStep = 0;
+
+  steps = input<StepData[]>([]);
+
+  goToStep(index: number) {
+    if (index >= 0 && index < this.steps().length) {
+      this.currentStep = index;
+      console.log(`set index ${index}`);
+    } else {
+      console.log(`could not set index ${index} ${this.steps().length}`);
+    }
+  }
+
+  getDataFromStepperButtonGroup(steps: StepButton[], action: 'prev' | 'next', index: number): ButtonGroupData {
+    return {
+      direction: 'horizontal',
+      buttons: steps.map(button => {
+        return {
+          ...button,
+          onClick: () => {
+            console.log(action + ' index ' + index);
+            button.onClick();
+            action === 'next' ? this.goToStep(index + 1) : this.goToStep(index - 1); //TODO
+          },
+        };
+      }),
+    };
+  }
+}
