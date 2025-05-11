59d5cacd45c10335c2f78ee2c5d9df527d26dd16
Ishani Budhwar <ge87tev@mytum.de>
2025-05-11 03:00:56 +0200

    Fix: Remove unnecessary comments

--- src/main/webapp/app/shared/components/atoms/datepicker/datepicker.component.ts
+++ src/main/webapp/app/shared/components/atoms/datepicker/datepicker.component.ts
@@ -45,25 +45,3 @@ export class DatePickerComponent {
     }
   }
 }
-
-// Testing the DatePickerComponent
-
-// import {Component} from '@angular/core';
-// import {DatePickerModule} from 'primeng/datepicker';
-// import {FormsModule} from '@angular/forms';
-// import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
-// import {faCalendar} from '@fortawesome/free-solid-svg-icons';
-
-// @Component({
-//     selector: 'jhi-datepicker',
-//     standalone: true,
-//     imports: [DatePickerModule, FormsModule, FontAwesomeModule],
-//     templateUrl: './datepicker.component.html',
-//     styleUrls: ['./datepicker.component.scss'],
-// })
-// export class DatePickerComponent {
-//     readonly faCalendar = faCalendar;
-//     selectedDate: Date | null = null;
-// fa: any;
-// solid: any;
-// }
