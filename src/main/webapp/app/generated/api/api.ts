a1b3194632844da5baac1750e1b9faeb684e6ddc
aniruddhzaveri <aniruddh.zaveri@tum.de>
2025-05-11 15:43:29 +0200

    test openapi

---
+++ src/main/webapp/app/generated/api/api.ts
@@ -0,0 +1,7 @@
+export * from './applicationEvaluationResource.service';
+import { ApplicationEvaluationResourceService } from './applicationEvaluationResource.service';
+export * from './applicationResource.service';
+import { ApplicationResourceService } from './applicationResource.service';
+export * from './jobResource.service';
+import { JobResourceService } from './jobResource.service';
+export const APIS = [ApplicationEvaluationResourceService, ApplicationResourceService, JobResourceService];
