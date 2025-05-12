4989e239110db25fcb3f52ecaca8ff7ddc7426c6
Marc Fett <marc.fett@web.de>
2025-05-13 01:33:22 +0200

    feat: add custom exception classes for improved error handling

---
+++ src/main/java/de/tum/cit/aet/core/exception/InternalServerException.java
@@ -0,0 +1,12 @@
+package de.tum.cit.aet.core.exception;
+
+public class InternalServerException extends RuntimeException {
+
+    public InternalServerException(String message) {
+        super(message);
+    }
+
+    public InternalServerException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
