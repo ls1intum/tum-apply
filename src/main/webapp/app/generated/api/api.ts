3467e1ec97ad02e9a044a484b4726429b2351575
aniruddhzaveri <aniruddh.zaveri@tum.de>
2025-05-24 16:39:59 +0200

    openapi fix

--- src/main/webapp/app/generated/api/api.ts
+++ src/main/webapp/app/generated/api/api.ts
@@ -1,17 +1,7 @@
-export * from './actuator.service';
-import { ActuatorService } from './actuator.service';
 export * from './applicationEvaluationResource.service';
 import { ApplicationEvaluationResourceService } from './applicationEvaluationResource.service';
 export * from './applicationResource.service';
 import { ApplicationResourceService } from './applicationResource.service';
 export * from './jobResource.service';
 import { JobResourceService } from './jobResource.service';
-export * from './userResource.service';
-import { UserResourceService } from './userResource.service';
-export const APIS = [
-  ActuatorService,
-  ApplicationEvaluationResourceService,
-  ApplicationResourceService,
-  JobResourceService,
-  UserResourceService,
-];
+export const APIS = [ApplicationEvaluationResourceService, ApplicationResourceService, JobResourceService];
