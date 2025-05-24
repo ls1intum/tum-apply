32cd0d06bc131c122334b64f558d93ff0c3fc1f1
Ishani Budhwar <ge87tev@mytum.de>
2025-05-24 16:05:10 +0200

    Fix: Add @NotNull to DTO

--- src/main/java/de/tum/cit/aet/job/dto/JobCardDTO.java
+++ src/main/java/de/tum/cit/aet/job/dto/JobCardDTO.java
@@ -2,17 +2,18 @@ package de.tum.cit.aet.job.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import de.tum.cit.aet.job.constants.Campus;
+import jakarta.validation.constraints.NotNull;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.util.UUID;
 
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public record JobCardDTO(
-    UUID jobId,
+    @NotNull UUID jobId,
     String title,
     String fieldOfStudies,
     Campus location,
-    UUID professorId,
+    @NotNull UUID professorId,
     Integer workload,
     LocalDate startDate,
     Instant createdAt
