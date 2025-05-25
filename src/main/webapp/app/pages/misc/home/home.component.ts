d65adbeeb0aecb1f0ee837c76f0a69f88ce2058a
Sarah Douglas <sarah.douglas@gmx.de>
2025-05-25 13:09:55 +0200

    feat: easier testing methods

--- src/main/webapp/app/pages/misc/home/home.component.ts
+++ src/main/webapp/app/pages/misc/home/home.component.ts
@@ -5,6 +5,7 @@ import { LoginService } from 'app/pages/usermanagement/login/login.service';
 import { AccountService } from 'app/core/auth/account.service';
 import { Account } from 'app/core/auth/account.model';
 import { ButtonComponent } from 'app/shared/components/atoms/button/button.component';
+import { ApplicationResourceService } from 'app/generated/api/applicationResource.service';
 
 @Component({
   selector: 'jhi-home',
@@ -17,11 +18,24 @@ export default class HomeComponent implements OnInit {
 
   account = signal<Account | null>(null);
 
+  application_exists = signal<boolean>(true);
+
   private readonly accountService = inject(AccountService);
   private readonly loginService = inject(LoginService);
+  private readonly applicationService = inject(ApplicationResourceService);
 
   ngOnInit(): void {
     this.accountService.identity().subscribe(account => this.account.set(account));
+    const application_exists = this.application_exists;
+    this.applicationService.getApplicationById('bce85235-25b7-46c2-988c-9290be79dc57').subscribe({
+      next() {
+        application_exists.set(true);
+      },
+      error(err) {
+        console.error(err);
+        application_exists.set(false);
+      },
+    });
   }
 
   login(): void {
