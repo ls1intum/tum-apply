3b00dad59d79caf43d7ae71092ae3772ba6cd72d
Marc Fett <marc.fett@web.de>
2025-05-27 20:11:54 +0200

    add new tests for auth-card

--- src/main/webapp/app/shared/components/organisms/auth-card/auth-card.component.spec.ts
+++ src/main/webapp/app/shared/components/organisms/auth-card/auth-card.component.spec.ts
@@ -1,14 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { faApple, faGoogle, faMicrosoft } from '@fortawesome/free-brands-svg-icons';
-
-import { MockKeycloakService } from '../../../../core/auth/keycloak.service.mock';
+import { keycloakService } from 'app/core/auth/keycloak.service';
 
 import { AuthCardComponent } from './auth-card.component';
 
-jest.mock('app/core/auth/keycloak.service', () => ({
-  keycloakService: new MockKeycloakService(),
-}));
+jest.mock('app/core/auth/keycloak.service', () => {
+  const { MockKeycloakService } = jest.requireActual('../../../../core/auth/keycloak.service.mock');
+  return {
+    keycloakService: new MockKeycloakService(),
+  };
+});
 
 describe('AuthCardComponent', () => {
   let component: AuthCardComponent;
@@ -31,5 +33,31 @@ describe('AuthCardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show correct Microsoft label in login mode', () => {
+    Object.defineProperty(component, 'mode', { get: () => () => 'login' });
+    fixture.detectChanges();
+    const btnData = component.studentButtons().buttons[0];
+    expect(btnData.label).toBe('Sign in with Microsoft');
+  });
+
+  it('should show correct Microsoft label in register mode', () => {
+    Object.defineProperty(component, 'mode', { get: () => () => 'register' });
+    fixture.detectChanges();
+    const btnData = component.studentButtons().buttons[0];
+    expect(btnData.label).toBe('Register with Microsoft');
+  });
+
+  it('should call authTabService.setSelectedTab when onTabChange is called', () => {
+    const authTabServiceSpy = jest.spyOn(component.authTabService, 'setSelectedTab');
+    component.onTabChange(1);
+    expect(authTabServiceSpy).toHaveBeenCalledWith(1);
+  });
+
+  it('should call keycloakService.login when onTUMSSOLogin is called', () => {
+    const keycloakServiceSpy = jest.spyOn(keycloakService, 'login');
+    component.onTUMSSOLogin();
+    expect(keycloakServiceSpy).toHaveBeenCalled();
   });
 });
