f16ec5508482c6da6acf869f99ffdf28cb278a4d
Marc Fett <marc.fett@web.de>
2025-05-29 16:11:01 +0200

    add new header component

--- src/main/webapp/app/layouts/main/main.component.ts
+++ src/main/webapp/app/layouts/main/main.component.ts
@@ -7,12 +7,13 @@ import { AppPageTitleStrategy } from 'app/app-page-title-strategy';
 
 import FooterComponent from '../footer/footer.component';
 import PageRibbonComponent from '../profiles/page-ribbon.component';
+import { HeaderComponent } from '../../shared/components/organisms/header/header.component';
 
 @Component({
   selector: 'jhi-main',
   templateUrl: './main.component.html',
   providers: [AppPageTitleStrategy],
-  imports: [RouterOutlet, FooterComponent, PageRibbonComponent],
+  imports: [HeaderComponent, RouterOutlet, FooterComponent, PageRibbonComponent],
 })
 export default class MainComponent implements OnInit {
   private readonly renderer: Renderer2;
