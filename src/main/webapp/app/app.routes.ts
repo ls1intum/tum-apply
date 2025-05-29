c21026e09ad218b4de9a875e6faf796a5e75f430
Marc Fett <marc.fett@web.de>
2025-05-29 16:00:28 +0200

    remove old component

--- src/main/webapp/app/app.routes.ts
+++ src/main/webapp/app/app.routes.ts
@@ -16,11 +16,6 @@ const routes: Routes = [
     title: 'home.title',
   },
   {
-    path: '',
-    loadComponent: () => import('./layouts/navbar/navbar.component'),
-    outlet: 'navbar',
-  },
-  {
     path: 'application/create/:job_id',
     loadComponent: () => import('./pages/application/application-creation/application-creation-form/application-creation-form.component'),
   },
