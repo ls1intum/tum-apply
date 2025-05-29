eb9ce35808657c6314053792254dea00b612d56c
Marc Fett <marc.fett@web.de>
2025-05-29 16:40:55 +0200

    add tum logo and change color scheme icon

--- src/main/webapp/app/shared/components/organisms/header/header.component.ts
+++ src/main/webapp/app/shared/components/organisms/header/header.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ButtonComponent } from '../../atoms/button/button.component';
 
 @Component({
   selector: 'jhi-header',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ButtonComponent],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss',
+  styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  isDarkMode = signal(document.body.classList.contains('tum-apply-dark-mode'));
+
+  toggleColorScheme(): void {
+    const className = 'tum-apply-dark-mode';
+    document.body.classList.toggle(className);
+    this.isDarkMode.set(document.body.classList.contains(className));
+  }
+}
