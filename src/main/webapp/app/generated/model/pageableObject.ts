a947fa3a045a29ad6765c4467a07f1b3a64d6796
Sarah Douglas <sarah.douglas@gmx.de>
2025-06-01 10:23:57 +0200

    chore: openapi

--- src/main/webapp/app/generated/model/pageableObject.ts
+++ src/main/webapp/app/generated/model/pageableObject.ts
@@ -13,9 +13,9 @@ import { SortObject } from './sortObject';
 export interface PageableObject { 
     offset?: number;
     sort?: SortObject;
-    unpaged?: boolean;
     pageSize?: number;
     pageNumber?: number;
+    unpaged?: boolean;
     paged?: boolean;
 }
 
