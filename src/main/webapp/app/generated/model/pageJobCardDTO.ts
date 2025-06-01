81260f1c8232870cdc37d2c70f434ddfded32a1b
Sarah Douglas <sarah.douglas@gmx.de>
2025-06-01 18:32:45 +0200

    chore: openapi

--- src/main/webapp/app/generated/model/pageJobCardDTO.ts
+++ src/main/webapp/app/generated/model/pageJobCardDTO.ts
@@ -13,16 +13,16 @@ import { SortObject } from './sortObject';
 
 
 export interface PageJobCardDTO { 
-    totalPages?: number;
     totalElements?: number;
-    first?: boolean;
-    last?: boolean;
+    totalPages?: number;
     size?: number;
     content?: Array<JobCardDTO>;
     number?: number;
     sort?: SortObject;
-    pageable?: PageableObject;
+    first?: boolean;
+    last?: boolean;
     numberOfElements?: number;
+    pageable?: PageableObject;
     empty?: boolean;
 }
 
