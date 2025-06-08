a481fc55e8f8fa0726fa595764d88694a7165f01
aniruddhzaveri <aniruddh.zaveri@tum.de>
2025-06-08 13:13:53 +0200

    filterchain

--- src/main/java/de/tum/cit/aet/core/config/SecurityConfiguration.java
+++ src/main/java/de/tum/cit/aet/core/config/SecurityConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -46,80 +47,8 @@ public class SecurityConfiguration {
      * @throws Exception if an error occurs
      */
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-            // Disables CSRF (Cross-Site Request Forgery) protection; useful in stateless APIs where the token management is unnecessary.
-            .csrf(CsrfConfigurer::disable)
-            // Adds a CORS (Cross-Origin Resource Sharing) filter before the username/password authentication to handle cross-origin requests.
-            .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
-            // Adds a custom filter for Single Page Applications (SPA), i.e. the client, after the basic authentication filter.
-            .addFilterAfter(new SpaWebFilter(), BasicAuthenticationFilter.class)
-            // Configures security headers.
-            .headers(headers ->
-                headers
-                    // Sets Content Security Policy (CSP) directives to prevent XSS attacks.
-                    .contentSecurityPolicy(csp -> csp.policyDirectives("script-src 'self' 'unsafe-inline' 'unsafe-eval'"))
-                    // Prevents the website from being framed, avoiding clickjacking attacks.
-                    .frameOptions(HeadersConfigurer.FrameOptionsConfig::deny)
-                    // Sets Referrer Policy to limit the amount of referrer information sent with requests.
-                    .referrerPolicy(referrer -> referrer.policy(ReferrerPolicyHeaderWriter.ReferrerPolicy.STRICT_ORIGIN_WHEN_CROSS_ORIGIN))
-                    // Disables HTTP Strict Transport Security as it is managed at the reverse proxy level (typically nginx).
-                    .httpStrictTransportSecurity((HeadersConfigurer.HstsConfig::disable))
-                    // Defines Permissions Policy to restrict what features the browser is allowed to use.
-                    .permissionsPolicyHeader(permissions ->
-                        permissions.policy(
-                            "camera=(), fullscreen=(*), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), midi=(), payment=(), sync-xhr=()"
-                        )
-                    )
-            )
-            // Configures sessions to be stateless; appropriate for REST APIs where no session is required.
-            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            // Configures authorization for various URL patterns. The patterns are considered in order.
-            .authorizeHttpRequests(requests ->
-                requests
-                    // NOTE: Always have a look at {@link de.tum.cit.aet.artemis.core.security.filter.SpaWebFilter} to see which URLs are forwarded to the SPA
-                    // Client related URLs and publicly accessible information (allowed for everyone).
-                    .requestMatchers("/", "/index.html", "/public/**")
-                    .permitAll()
-                    .requestMatchers("/*.js", "/*.css", "/*.map", "/*.json")
-                    .permitAll()
-                    .requestMatchers("/manifest.webapp", "/robots.txt")
-                    .permitAll()
-                    .requestMatchers("/content/**", "/i18n/*.json", "/logo/*")
-                    .permitAll()
-                    // Information and health endpoints do not need authentication
-                    .requestMatchers("/management/info", "/management/health")
-                    .permitAll()
-                    // Admin area requires specific authority.
-                    .requestMatchers("/api/*/admin/**")
-                    .hasAuthority("ADMIN")
-                    // Publicly accessible API endpoints (allowed for everyone).
-                    .requestMatchers("/api/*/public/**")
-                    .permitAll()
-                    .requestMatchers("/api/**")
-                    .authenticated()
-                    .requestMatchers("/login/webauthn")
-                    .permitAll()
-                    // Websocket and other specific endpoints allowed without authentication.
-                    .requestMatchers("/websocket/**")
-                    .permitAll()
-                    .requestMatchers("/.well-known/jwks.json")
-                    .permitAll()
-                    .requestMatchers("/.well-known/assetlinks.json")
-                    .permitAll()
-                    // Prometheus endpoint protected by IP address.
-                    .requestMatchers("/management/prometheus/**")
-                    .permitAll()
-                    .requestMatchers(("/api-docs"))
-                    .permitAll()
-                    .requestMatchers(("/api-docs.yaml"))
-                    .permitAll()
-                    .requestMatchers("/swagger-ui/**")
-                    .permitAll()
-                    .requestMatchers(HttpMethod.POST, "/api/jobs")
-                    .permitAll()
-            )
-            .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(customJwtAuthenticationConverter)));
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(authz -> authz.anyRequest().permitAll());
         return http.build();
     }
 
