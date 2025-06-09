0774b3c7e66ce57da0bc6cc2b02f7f7c928dd9f8
Marc Fett <marc.fett@web.de>
2025-06-09 23:45:34 +0200

    translate voices section

--- src/main/webapp/app/shared/pages/landing-page/voices-section/voices-section.component.ts
+++ src/main/webapp/app/shared/pages/landing-page/voices-section/voices-section.component.ts
@@ -1,45 +1,45 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
 
 import { VoiceCardComponent } from './voice-card/voice-card/voice-card.component';
 
 @Component({
   selector: 'jhi-voices-section',
   standalone: true,
-  imports: [CommonModule, VoiceCardComponent],
+  imports: [TranslateModule, VoiceCardComponent],
   templateUrl: './voices-section.component.html',
   styleUrl: './voices-section.component.scss',
 })
 export class VoicesSectionComponent {
   voices = [
     {
-      name: 'Aline Vogt',
-      field: 'Mathematics',
-      quote: 'TUM offers a unique opportunity for research.',
+      name: 'landingPage.voices.testimonials.1.name',
+      field: 'landingPage.voices.testimonials.1.field',
+      quote: 'landingPage.voices.testimonials.1.quote',
       imageSrc: '/content/images/landing-page/voices-section/aline-vogt.png',
     },
     {
-      name: 'Lukas Müller',
-      field: 'Informatics',
-      quote: 'I appreciate the support and network at TUM.',
+      name: 'landingPage.voices.testimonials.2.name',
+      field: 'landingPage.voices.testimonials.2.field',
+      quote: 'landingPage.voices.testimonials.2.quote',
       imageSrc: '/content/images/landing-page/voices-section/lukas-mueller.png',
     },
     {
-      name: 'Dr. Sofia Klein',
-      field: 'Biomedical Engineering',
-      quote: 'The diversity at TUM is inspiring.',
+      name: 'landingPage.voices.testimonials.3.name',
+      field: 'landingPage.voices.testimonials.3.field',
+      quote: 'landingPage.voices.testimonials.3.quote',
       imageSrc: '/content/images/landing-page/voices-section/sofia-klein.png',
     },
     {
-      name: 'Max Richter',
-      field: 'Chemistry',
-      quote: 'TUM is a leader in innovation and research.',
+      name: 'landingPage.voices.testimonials.4.name',
+      field: 'landingPage.voices.testimonials.4.field',
+      quote: 'landingPage.voices.testimonials.4.quote',
       imageSrc: '/content/images/landing-page/voices-section/max-richter.png',
     },
     {
-      name: 'Emily Wagner',
-      field: 'Computer Vision',
-      quote: 'I feel welcomed and supported here.',
+      name: 'landingPage.voices.testimonials.5.name',
+      field: 'landingPage.voices.testimonials.5.field',
+      quote: 'landingPage.voices.testimonials.5.quote',
       imageSrc: '/content/images/landing-page/voices-section/emily-wagner.png',
     },
   ];
