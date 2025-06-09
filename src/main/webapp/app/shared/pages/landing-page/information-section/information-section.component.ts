9186235a8800403ebcb46281b3e674250864af0a
Marc Fett <marc.fett@web.de>
2025-06-09 17:19:21 +0200

    adapt information section to components

--- src/main/webapp/app/shared/pages/landing-page/information-section/information-section.component.ts
+++ src/main/webapp/app/shared/pages/landing-page/information-section/information-section.component.ts
@@ -1,9 +1,45 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+
+import { InformationCardComponent } from './information-card/information-card/information-card.component';
 
 @Component({
   selector: 'jhi-information-section',
-  imports: [],
+  imports: [InformationCardComponent, TranslateModule],
   templateUrl: './information-section.component.html',
   styleUrl: './information-section.component.scss',
 })
-export class InformationSectionComponent {}
+export class InformationSectionComponent {
+  cards = [
+    {
+      imageSrc: '/content/images/landing-page/information-section/excellence.png',
+      text: 'landingPage.informationSection.tiles.1',
+      link: 'https://www.tum.de/en/about-tum/university-of-excellence',
+    },
+    {
+      imageSrc: '/content/images/landing-page/information-section/events.png',
+      text: 'landingPage.informationSection.tiles.2',
+      link: 'https://www.gs.tum.de/gs/veranstaltungen/',
+    },
+    {
+      imageSrc: '/content/images/landing-page/information-section/women.png',
+      text: 'landingPage.informationSection.tiles.3',
+      link: 'https://www.community.tum.de/en/tum-community/women/',
+    },
+    {
+      imageSrc: '/content/images/landing-page/information-section/internationality.png',
+      text: 'landingPage.informationSection.tiles.4',
+      link: 'https://www.tum.de/en/studies/international-students',
+    },
+    {
+      imageSrc: '/content/images/landing-page/information-section/funding.png',
+      text: 'landingPage.informationSection.tiles.5',
+      link: 'https://www.gs.tum.de/en/gs/path-to-a-doctorate/funding/',
+    },
+    {
+      imageSrc: '/content/images/landing-page/information-section/diversity.png',
+      text: 'landingPage.informationSection.tiles.6',
+      link: 'https://www.tum.de/en/about-tum/goals-and-values/diversity',
+    },
+  ];
+}
