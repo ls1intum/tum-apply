1561263e4d2df6f290deed52312a26c9aba3ad66
Marc Fett <marc.fett@web.de>
2025-06-09 14:19:17 +0200

    add doctoral-journey section

--- src/main/webapp/app/shared/pages/landing-page/doctoral-journey-section/doctoral-journey-section.component.ts
+++ src/main/webapp/app/shared/pages/landing-page/doctoral-journey-section/doctoral-journey-section.component.ts
@@ -1,9 +1,36 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+
+import ButtonGroupComponent, { ButtonGroupData } from '../../../components/molecules/button-group/button-group.component';
 
 @Component({
   selector: 'jhi-doctoral-journey-section',
-  imports: [],
+  imports: [CommonModule, TranslateModule, ButtonGroupComponent],
   templateUrl: './doctoral-journey-section.component.html',
   styleUrl: './doctoral-journey-section.component.scss',
 })
-export class DoctoralJourneySectionComponent {}
+export class DoctoralJourneySectionComponent {
+  constructor(private translate: TranslateService) {}
+
+  buttons(): ButtonGroupData {
+    return {
+      direction: 'horizontal',
+      buttons: [
+        {
+          label: this.translate.instant('landingPage.doctoralJourney.button1'),
+          severity: 'secondary',
+          variant: 'outlined',
+          disabled: false,
+          onClick() {},
+        },
+        {
+          label: this.translate.instant('landingPage.doctoralJourney.button2'),
+          severity: 'primary',
+          disabled: false,
+          onClick() {},
+        },
+      ],
+    };
+  }
+}
