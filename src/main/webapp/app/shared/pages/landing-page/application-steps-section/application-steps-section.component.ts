3b59c3d25b167a1468ec12b2c7955f5499804613
Marc Fett <marc.fett@web.de>
2025-06-09 16:04:15 +0200

    adapt application step section to use application step component

--- src/main/webapp/app/shared/pages/landing-page/application-steps-section/application-steps-section.component.ts
+++ src/main/webapp/app/shared/pages/landing-page/application-steps-section/application-steps-section.component.ts
@@ -1,9 +1,37 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+
+import { ButtonComponent } from '../../../components/atoms/button/button.component';
+
+import { ApplicationStepComponent } from './application-step/application-step/application-step.component';
 
 @Component({
   selector: 'jhi-application-steps-section',
-  imports: [],
+  imports: [ApplicationStepComponent, ButtonComponent, TranslateModule],
   templateUrl: './application-steps-section.component.html',
   styleUrl: './application-steps-section.component.scss',
 })
-export class ApplicationStepsSectionComponent {}
+export class ApplicationStepsSectionComponent {
+  steps = [
+    {
+      icon: 'search',
+      title: 'landingPage.applicationSteps.steps.1.title',
+      description: 'landingPage.applicationSteps.steps.1.description',
+    },
+    {
+      icon: 'info',
+      title: 'landingPage.applicationSteps.steps.2.title',
+      description: 'landingPage.applicationSteps.steps.2.description',
+    },
+    {
+      icon: 'paper-plane',
+      title: 'landingPage.applicationSteps.steps.3.title',
+      description: 'landingPage.applicationSteps.steps.3.description',
+    },
+    {
+      icon: 'bell',
+      title: 'landingPage.applicationSteps.steps.4.title',
+      description: 'landingPage.applicationSteps.steps.4.description',
+    },
+  ];
+}
