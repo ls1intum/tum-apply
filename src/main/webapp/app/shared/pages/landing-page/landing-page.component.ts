d84b0f32cf756ab1c9d2ede83c0e32a6be1aab1a
Marc Fett <marc.fett@web.de>
2025-06-09 10:48:15 +0200

    change landing page component

--- src/main/webapp/app/shared/pages/landing-page/landing-page.component.ts
+++ src/main/webapp/app/shared/pages/landing-page/landing-page.component.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
 
+import { HeroSectionComponent } from './hero-section/hero-section.component';
+import { ApplicationStepsSectionComponent } from './application-steps-section/application-steps-section.component';
+import { DoctoralJourneySectionComponent } from './doctoral-journey-section/doctoral-journey-section.component';
+import { InformationSectionComponent } from './information-section/information-section.component';
+import { VoicesSectionComponent } from './voices-section/voices-section.component';
+import { FaqSectionComponent } from './faq-section/faq-section.component';
+
 @Component({
   selector: 'jhi-landing-page',
-  imports: [],
+  imports: [
+    HeroSectionComponent,
+    ApplicationStepsSectionComponent,
+    DoctoralJourneySectionComponent,
+    InformationSectionComponent,
+    VoicesSectionComponent,
+    FaqSectionComponent,
+  ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss',
 })
