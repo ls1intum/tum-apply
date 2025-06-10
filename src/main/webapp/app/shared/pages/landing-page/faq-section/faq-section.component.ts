d885d1657a3cb56dbac971662ac753f2fb05d7f8
Marc Fett <marc.fett@web.de>
2025-06-10 11:18:32 +0200

    adapt faq section

--- src/main/webapp/app/shared/pages/landing-page/faq-section/faq-section.component.ts
+++ src/main/webapp/app/shared/pages/landing-page/faq-section/faq-section.component.ts
@@ -1,9 +1,35 @@
 import { Component } from '@angular/core';
+import { AccordionModule } from 'primeng/accordion';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'jhi-faq-section',
-  imports: [],
+  standalone: true,
+  imports: [AccordionModule, TranslateModule],
   templateUrl: './faq-section.component.html',
   styleUrl: './faq-section.component.scss',
 })
-export class FaqSectionComponent {}
+export class FaqSectionComponent {
+  tabs = [
+    {
+      value: 'login',
+      title: 'landingPage.faq.questions.login.title',
+      content: 'landingPage.faq.questions.login.content',
+    },
+    {
+      value: 'multiple-applications',
+      title: 'landingPage.faq.questions.multipleApplications.title',
+      content: 'landingPage.faq.questions.multipleApplications.content',
+    },
+    {
+      value: 'documents',
+      title: 'landingPage.faq.questions.documents.title',
+      content: 'landingPage.faq.questions.documents.content',
+    },
+    {
+      value: 'status',
+      title: 'landingPage.faq.questions.status.title',
+      content: 'landingPage.faq.questions.status.content',
+    },
+  ];
+}
