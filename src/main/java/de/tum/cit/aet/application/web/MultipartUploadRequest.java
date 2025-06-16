529c61ad587e6187931de082d1c5b0b226c8f529
Sarah Douglas <sarah.douglas@gmx.de>
2025-06-16 14:07:53 +0200

    fix: replaced getter and setter

--- src/main/java/de/tum/cit/aet/application/web/MultipartUploadRequest.java
+++ src/main/java/de/tum/cit/aet/application/web/MultipartUploadRequest.java
@@ -2,18 +2,14 @@ package de.tum.cit.aet.application.web;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 
+@Getter
+@Setter
 public class MultipartUploadRequest {
 
     @Schema(type = "array", format = "binary", description = "List of documents to upload")
     private List<MultipartFile> files;
-
-    public List<MultipartFile> getFiles() {
-        return files;
-    }
-
-    public void setFiles(List<MultipartFile> files) {
-        this.files = files;
-    }
 }
