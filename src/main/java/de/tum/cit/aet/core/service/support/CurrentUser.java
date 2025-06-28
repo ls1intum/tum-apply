b08acdeaf6655c85e7be82f429c892f5c83bf550
Marc Fett <marc.fett@web.de>
2025-06-28 16:29:31 +0200

    Add new records CurrentUser and ResearchGroupRole

---
+++ src/main/java/de/tum/cit/aet/core/service/support/CurrentUser.java
@@ -0,0 +1,26 @@
+package de.tum.cit.aet.core.service.support;
+
+import de.tum.cit.aet.usermanagement.constants.UserRole;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.UUID;
+
+public record CurrentUser(UUID userId, String email, String firstName, String lastName, List<ResearchGroupRole> researchGroupRoles) {
+    public boolean isAdmin() {
+        return researchGroupRoles.stream().anyMatch(r -> r.role() == UserRole.ADMIN);
+    }
+
+    public boolean isProfessor() {
+        return researchGroupRoles.stream().anyMatch(r -> r.role() == UserRole.PROFESSOR);
+    }
+
+    public Optional<UUID> getResearchGroupIdIfProfessor() {
+        return researchGroupRoles
+            .stream()
+            .filter(r -> r.role() == UserRole.PROFESSOR)
+            .map(ResearchGroupRole::researchGroupId)
+            .filter(Objects::nonNull)
+            .findFirst();
+    }
+}
