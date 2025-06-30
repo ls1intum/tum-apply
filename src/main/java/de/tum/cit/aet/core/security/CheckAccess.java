40aac8a08c875853c7ad00e8e7ac1796512d55fe
Marc Fett <marc.fett@web.de>
2025-06-30 16:35:32 +0200

    add javadocs to CheckAccess

--- src/main/java/de/tum/cit/aet/core/security/CheckAccess.java
+++ src/main/java/de/tum/cit/aet/core/security/CheckAccess.java
@@ -2,9 +2,23 @@ package de.tum.cit.aet.core.security;
 
 import java.lang.annotation.*;
 
+/**
+ * Annotation to mark a controller method for access control.
+ * The {@link AccessTarget} determines which argument should be validated for access,
+ * such as a research group ID or user ID.
+ * <p>
+ * This annotation is processed by {@link CheckAccessAspect} to ensure that
+ * the current user has the proper role and permissions before allowing method execution.
+ */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface CheckAccess {
+    /**
+     * Defines which method argument should be used for access control checks.
+     * Defaults to {@link AccessTarget#RESEARCH_GROUP_ID}.
+     *
+     * @return the access target type to check
+     */
     AccessTarget type() default AccessTarget.RESEARCH_GROUP_ID;
 }
