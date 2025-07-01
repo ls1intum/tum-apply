8fffee5db3ef32c227704f99be4eb375bdd7d379
Moritz <moritzschmidt1@gmail.com>
2025-07-01 13:31:20 +0200

    add default reject message

--- src/main/webapp/app/shared/components/molecules/review-dialog/review-dialog.component.ts
+++ src/main/webapp/app/shared/components/molecules/review-dialog/review-dialog.component.ts
@@ -1,14 +1,15 @@
-import { Component, ViewEncapsulation, computed, effect, input, model, output, signal } from '@angular/core';
+import { Component, ViewEncapsulation, computed, effect, inject, input, model, output, signal } from '@angular/core';
 import { DialogModule } from 'primeng/dialog';
 import { CheckboxModule } from 'primeng/checkbox';
 import { NgTemplateOutlet } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 import { DropdownComponent, DropdownOption } from '../../atoms/dropdown/dropdown.component';
 import { ButtonComponent } from '../../atoms/button/button.component';
 import { EditorComponent } from '../../atoms/editor/editor.component';
+import { ApplicationEvaluationDetailDTO } from '../../../../generated';
 
 @Component({
   selector: 'jhi-review-dialog',
@@ -29,14 +30,17 @@ import { EditorComponent } from '../../atoms/editor/editor.component';
   encapsulation: ViewEncapsulation.None,
 })
 export class ReviewDialogComponent {
-  applicantName = input<string | undefined>(undefined);
-  jobName = input<string | undefined>(undefined);
+  translate = inject(TranslateService);
+
   mode = input.required<'ACCEPT' | 'REJECT'>();
   visible = model<boolean>(false);
 
+  application = input<ApplicationEvaluationDetailDTO | undefined>(undefined);
+
   notifyApplicant = model<boolean>(true);
   closeJob = model<boolean>(false);
-  message = model<string | undefined>(undefined);
+
+  editorModel = signal<string>('');
 
   selectedRejectReason = signal<DropdownOption | undefined>(undefined);
 
@@ -44,11 +48,27 @@ export class ReviewDialogComponent {
   reject = output();
 
   canAccept = computed(() => {
-    return (this.notifyApplicant() && this.message() !== undefined && this.message() !== '') || !this.notifyApplicant();
+    return (this.notifyApplicant() && this.editorModel().length > 7) || !this.notifyApplicant();
   });
 
   canReject = computed(() => {
     return this.selectedRejectReason() !== undefined;
+  });
+
+  translationMetaData = computed(() => {
+    const application = this.application();
+
+    if (application) {
+      return {
+        APPLICANT_FIRST_NAME: application.applicationDetailDTO.applicant?.user.name ?? '',
+        PROFESSOR_EMAIL: application.professor?.email,
+        JOB_NAME: application.applicationDetailDTO.jobTitle,
+        PROFESSOR_FIRST_NAME: application.professor?.firstName,
+        PROFESSOR_LAST_NAME: application.professor?.lastName,
+        RESEARCH_GROUP_NAME: application.professor?.researchGroupName,
+      };
+    }
+    return undefined;
   });
 
   rejectReasons: DropdownOption[] = [
@@ -74,16 +94,12 @@ export class ReviewDialogComponent {
     effect(() => {
       if (this.visible()) {
         this.resetDialogState();
+        this.editorModel.set(this.translate.instant('evaluation.defaultRejectMessage', this.translationMetaData()));
       }
     });
   }
 
-  onMessageChange(text: string): void {
-    this.message.set(text);
-  }
-
   resetDialogState(): void {
-    this.message.set(undefined);
     this.notifyApplicant.set(true);
     this.closeJob.set(false);
     this.selectedRejectReason.set(undefined);
@@ -91,5 +107,9 @@ export class ReviewDialogComponent {
 
   onSelectChange(option: DropdownOption): void {
     this.selectedRejectReason.set(option);
+  }
+
+  onAccept(): void {
+    console.warn(this.editorModel());
   }
 }
