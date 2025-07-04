91b5571ea5eb73434ed4138765b7f7c6e70ccba2
Kiara Copony <ge65min@mytum.de>
2025-07-04 22:23:02 +0200

    add privacy page component

---
+++ src/main/webapp/app/shared/pages/privacy-page/privacy-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jhi-privacy-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './privacy-page.component.html',
+  styleUrl: './privacy-page.component.scss',
+})
+export class PrivacyPageComponent {}
