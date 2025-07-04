2f6dea8b6a694167104e83e1771dd4291b1a2835
Kiara Copony <ge65min@mytum.de>
2025-07-04 22:23:21 +0200

    add imprint page component

---
+++ src/main/webapp/app/shared/pages/imprint-page/imprint-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jhi-imprint-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './imprint-page.component.html',
+  styleUrl: './imprint-page.component.scss',
+})
+export class ImprintPageComponent {}
