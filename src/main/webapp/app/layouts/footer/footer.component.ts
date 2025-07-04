5398927ea4c1c61fca40bcc10889b495f9503608
Kiara Copony <ge65min@mytum.de>
2025-07-04 18:51:10 +0200

    add translation to footer

--- src/main/webapp/app/layouts/footer/footer.component.ts
+++ src/main/webapp/app/layouts/footer/footer.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { TranslateDirective } from 'app/shared/language';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'jhi-footer',
+  standalone: true,
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
-  imports: [TranslateDirective],
+  imports: [TranslateDirective, TranslateModule],
 })
-export default class FooterComponent {}
+export default class FooterComponent {
+  constructor(private translateService: TranslateService) {}
+}
