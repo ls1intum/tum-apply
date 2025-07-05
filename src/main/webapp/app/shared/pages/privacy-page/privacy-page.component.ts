4ba4e5bc06142be470c6d8362106f645eb984b8f
Kiara Copony <ge65min@mytum.de>
2025-07-05 20:42:43 +0200

    create privacy page

--- src/main/webapp/app/shared/pages/privacy-page/privacy-page.component.ts
+++ src/main/webapp/app/shared/pages/privacy-page/privacy-page.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 
+import TranslateDirective from '../../language/translate.directive';
+
 @Component({
   selector: 'jhi-privacy-page',
   standalone: true,
-  imports: [],
+  imports: [TranslateDirective],
   templateUrl: './privacy-page.component.html',
   styleUrl: './privacy-page.component.scss',
 })
