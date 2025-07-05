74d6f03f11b39d9ba2bd6f56ba7449860fe5be81
Kiara Copony <ge65min@mytum.de>
2025-07-05 09:14:41 +0200

    use jhiTranslatate instead of pipeline

--- src/main/webapp/app/layouts/footer/footer.component.ts
+++ src/main/webapp/app/layouts/footer/footer.component.ts
@@ -1,20 +1,16 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { TranslateDirective } from 'app/shared/language';
-import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import TranslateDirective from '../../shared/language/translate.directive';
 
 @Component({
   selector: 'jhi-footer',
   standalone: true,
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
-  imports: [TranslateDirective, TranslateModule],
+  imports: [TranslateDirective],
 })
 export default class FooterComponent {
-  constructor(
-    private translateService: TranslateService,
-    private router: Router,
-  ) {}
+  constructor(private router: Router) {}
 
   navigateToImprint(): void {
     void this.router.navigate(['/imprint']);
