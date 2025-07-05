adcc426dee2fe1ceb605ec9458894587b1014745
Sarah Douglas <sarah.douglas@gmx.de>
2025-07-05 14:49:43 +0200

    fix: multiple document viewer

--- src/main/webapp/app/shared/components/atoms/document-viewer/document-viewer.component.ts
+++ src/main/webapp/app/shared/components/atoms/document-viewer/document-viewer.component.ts
@@ -1,4 +1,5 @@
 import { Component, effect, inject, input, signal } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { DocumentResourceService } from 'app/generated';
 import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
 import { firstValueFrom } from 'rxjs';
@@ -14,17 +15,21 @@ export class DocumentViewerComponent {
   documentDictionaryId = input.required<string>();
 
   pdfSrc = signal<Blob | null>(null);
+  sanitizedBlobUrl?: SafeResourceUrl;
 
   private documentService = inject(DocumentResourceService);
 
-  constructor() {
+  constructor(private sanitizer: DomSanitizer) {
     effect(() => {
       this.initDocument();
     });
   }
 
-  async initDocument(): Promise<void> {
-    const pdfSrc = await firstValueFrom(this.documentService.downloadDocument(this.documentDictionaryId()));
-    this.pdfSrc.set(pdfSrc);
+  async initDocument() {
+    const response = await firstValueFrom(this.documentService.downloadDocument(this.documentDictionaryId()));
+    const pdfBlob = new Blob([response], { type: 'application/pdf' });
+    this.pdfSrc.set(pdfBlob);
+    const blobUrl = URL.createObjectURL(pdfBlob);
+    this.sanitizedBlobUrl = this.sanitizer.bypassSecurityTrustResourceUrl(blobUrl + '#toolbar=0&navpanes=0');
   }
 }
