da28543123e9890f81a20c6c771170a3ef16decb
aniruddhzaveri <aniruddh.zaveri@tum.de>
2025-07-09 00:39:57 +0200

    reduce image size further

--- src/main/webapp/app/shared/pages/landing-page/information-section/information-section.component.ts
+++ src/main/webapp/app/shared/pages/landing-page/information-section/information-section.component.ts
@@ -14,32 +14,32 @@ import { InformationCardComponent } from './information-card/information-card/in
 export class InformationSectionComponent {
   cards = [
     {
-      imageSrc: '/content/images/landing-page/information-section/excellence.png',
+      imageSrc: '/content/images/landing-page/information-section/excellence.webp',
       text: 'landingPage.informationSection.tiles.1',
       link: 'https://www.tum.de/en/about-tum/university-of-excellence',
     },
     {
-      imageSrc: '/content/images/landing-page/information-section/events.png',
+      imageSrc: '/content/images/landing-page/information-section/events.webp',
       text: 'landingPage.informationSection.tiles.2',
       link: 'https://www.gs.tum.de/gs/veranstaltungen/',
     },
     {
-      imageSrc: '/content/images/landing-page/information-section/women.png',
+      imageSrc: '/content/images/landing-page/information-section/women.webp',
       text: 'landingPage.informationSection.tiles.3',
       link: 'https://www.community.tum.de/en/tum-community/women/',
     },
     {
-      imageSrc: '/content/images/landing-page/information-section/internationality.png',
+      imageSrc: '/content/images/landing-page/information-section/internationality.webp',
       text: 'landingPage.informationSection.tiles.4',
       link: 'https://www.tum.de/en/studies/international-students',
     },
     {
-      imageSrc: '/content/images/landing-page/information-section/funding.png',
+      imageSrc: '/content/images/landing-page/information-section/funding.webp',
       text: 'landingPage.informationSection.tiles.5',
       link: 'https://www.gs.tum.de/en/gs/path-to-a-doctorate/funding/',
     },
     {
-      imageSrc: '/content/images/landing-page/information-section/diversity.png',
+      imageSrc: '/content/images/landing-page/information-section/diversity.webp',
       text: 'landingPage.informationSection.tiles.6',
       link: 'https://www.zv.tum.de/en/diversity/homepage/',
     },
