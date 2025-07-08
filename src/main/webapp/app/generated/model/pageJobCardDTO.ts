295ec9a301cb05c8d1db52ef3d30d8fa9ffc28ff
Sarah Douglas <sarah.douglas@gmx.de>
2025-07-08 23:11:57 +0200

    fix: client test

--- src/main/webapp/app/generated/model/pageJobCardDTO.ts
+++ src/main/webapp/app/generated/model/pageJobCardDTO.ts
@@ -13,16 +13,16 @@ import { SortObject } from './sortObject';
 
 
 export interface PageJobCardDTO { 
-    totalPages?: number;
     totalElements?: number;
+    totalPages?: number;
     size?: number;
     content?: Array<JobCardDTO>;
     number?: number;
     sort?: SortObject;
-    numberOfElements?: number;
-    pageable?: PageableObject;
     first?: boolean;
     last?: boolean;
+    numberOfElements?: number;
+    pageable?: PageableObject;
     empty?: boolean;
 }
 
