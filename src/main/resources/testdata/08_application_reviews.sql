64eb6713c8452335dc8f9010c12bce80c52ac563
Ishani Budhwar <88331280+ani-ib@users.noreply.github.com>
2025-07-09 18:50:26 +0200

    `Development`: Update test data scripts for database (#255)
    * implement closing of jobs
    
    * add close button in "My Positions"
    
    * remove up unused methods
    
    * openapi and fix delete button
    
    * Fix rejecting job logic for saved applications and retrieve all applicants to notify
    
    * fix application state updating
    
    * add javadoc comments
    
    * Fix: client tests
    
    * retrieve job title and research group name for notifications
    
    * first version of test data
    
    * add html descriptions for research groups
    
    * add html descriptions for research groups
    
    * remove incorrect campus from jobs
    
    * clean up scripts
    
    * clean up unnecessary comments

--- src/main/resources/testdata/08_application_reviews.sql
+++ src/main/resources/testdata/08_application_reviews.sql
@@ -9,16 +9,16 @@
 -- clean existing data
 DELETE
 FROM application_reviews
-WHERE application_review_id IN ('fd7d62ea-9353-44df-b91c-170a74823aad',
-                                'ece2685c-f122-4879-a31f-ff879aaab9c0',
-                                '71e44d2e-9bb5-4424-ac2d-58be111c0083');
-
-
+WHERE application_review_id LIKE '820ea750-2c97-4e6b-9e2a-%';
 -- insert test data
-REPLACE INTO application_reviews (application_review_id, application_id, user_id, reason, reviewed_at)
-VALUES ('fd7d62ea-9353-44df-b91c-170a74823aad', '2438c612-6045-408c-8528-af4e99a52859',
-        '00000000-0000-0000-0000-000000000101', 'Strong academic background', '2025-04-20 00:00:00'),
-       ('ece2685c-f122-4879-a31f-ff879aaab9c0', '2a18ee8b-9f8d-4369-89fd-7d4768845efa',
-        '00000000-0000-0000-0000-000000000101', 'Lacks relevant experience', '2025-04-21 00:00:00'),
-       ('71e44d2e-9bb5-4424-ac2d-58be111c0083', 'f7ec007f-fba0-4135-b88c-c70eec79faf3',
-        '00000000-0000-0000-0000-000000000101', NULL, '2025-04-21 00:00:00');
+REPLACE INTO application_reviews (
+    application_review_id,
+    application_id,
+    user_id,
+    reason,
+    reviewed_at
+)
+VALUES
+  ('820ea750-2c97-4e6b-9e2a-000000000001', '00000000-0000-0000-0000-300000020003', '00000000-0000-0000-0000-000000000102', 'Strong match with project needs and experience.', '2025-11-01 11:50:09'),
+  ('820ea750-2c97-4e6b-9e2a-000000000003', '00000000-0000-0000-0000-300000023331', '00000000-0000-0000-0000-000000000102', 'Lacks relevant experience or academic alignment.', '2025-07-12 10:00:22'),
+  ('820ea750-2c97-4e6b-9e2a-000000000004', '00000000-0000-0000-0000-300000020837', '00000000-0000-0000-0000-000000000105', 'Lacks relevant experience or academic alignment.', '2025-02-01 11:02:11');
