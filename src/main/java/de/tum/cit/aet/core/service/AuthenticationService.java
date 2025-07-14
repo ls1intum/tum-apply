23334177d5cba1043eefba97f78438c8847bc935
Sarah Douglas <sarah.douglas@gmx.de>
2025-07-14 21:03:19 +0200

    fix-try: 2

--- src/main/java/de/tum/cit/aet/core/service/AuthenticationService.java
+++ src/main/java/de/tum/cit/aet/core/service/AuthenticationService.java
@@ -4,9 +4,11 @@ import de.tum.cit.aet.usermanagement.constants.UserRole;
 import de.tum.cit.aet.usermanagement.domain.Applicant;
 import de.tum.cit.aet.usermanagement.domain.User;
 import de.tum.cit.aet.usermanagement.domain.UserResearchGroupRole;
+import de.tum.cit.aet.usermanagement.repository.ApplicantRepository;
 import de.tum.cit.aet.usermanagement.repository.UserRepository;
 import de.tum.cit.aet.usermanagement.repository.UserResearchGroupRoleRepository;
 import java.util.Locale;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
@@ -16,11 +18,17 @@ import org.springframework.transaction.annotation.Transactional;
 public class AuthenticationService {
 
     private final UserRepository userRepository;
+    private final ApplicantRepository applicantRepository;
     private final UserResearchGroupRoleRepository userResearchGroupRoleRepository;
 
-    public AuthenticationService(UserRepository userRepository, UserResearchGroupRoleRepository userResearchGroupRoleRepository) {
+    public AuthenticationService(
+        UserRepository userRepository,
+        UserResearchGroupRoleRepository userResearchGroupRoleRepository,
+        ApplicantRepository applicantRepository
+    ) {
         this.userRepository = userRepository;
         this.userResearchGroupRoleRepository = userResearchGroupRoleRepository;
+        this.applicantRepository = applicantRepository;
     }
 
     /**
@@ -37,19 +45,22 @@ public class AuthenticationService {
         boolean hasRoles = userResearchGroupRoleRepository.existsByUserUserId(userId);
         User user;
         if (!hasRoles) {
-            user = userRepository
-                .findWithResearchGroupRolesByUserId(userId)
-                .orElseGet(() -> {
-                    Applicant newUser = new Applicant();
-                    newUser.setUserId(userId);
-                    newUser.setSelectedLanguage("en");
-                    return newUser;
-                });
-
-            user.setEmail(jwt.getClaimAsString("email").toLowerCase(Locale.ROOT));
-            user.setFirstName(jwt.getClaimAsString("given_name"));
-            user.setLastName(jwt.getClaimAsString("family_name"));
-            user = userRepository.save(user);
+            Optional<User> tempUser = userRepository.findWithResearchGroupRolesByUserId(userId);
+            if (tempUser.isEmpty()) {
+                Applicant newUser = new Applicant();
+                newUser.setUserId(userId);
+                newUser.setSelectedLanguage("en");
+                newUser.setEmail(jwt.getClaimAsString("email").toLowerCase(Locale.ROOT));
+                newUser.setFirstName(jwt.getClaimAsString("given_name"));
+                newUser.setLastName(jwt.getClaimAsString("family_name"));
+                user = applicantRepository.save(newUser);
+            } else {
+                user = tempUser.get();
+                user.setEmail(jwt.getClaimAsString("email").toLowerCase(Locale.ROOT));
+                user.setFirstName(jwt.getClaimAsString("given_name"));
+                user.setLastName(jwt.getClaimAsString("family_name"));
+                user = userRepository.save(user);
+            }
 
             UserResearchGroupRole defaultRole = new UserResearchGroupRole();
             defaultRole.setUser(user);
