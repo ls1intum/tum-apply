ac02bab70897b907309d6fe98c48ff0047605c89
Marc Fett <marc.fett@web.de>
2025-07-14 02:29:58 +0200

    add cookie and securities

--- src/main/java/de/tum/cit/aet/usermanagement/web/EmailLoginResource.java
+++ src/main/java/de/tum/cit/aet/usermanagement/web/EmailLoginResource.java
@@ -1,9 +1,12 @@
 package de.tum.cit.aet.usermanagement.web;
 
 import de.tum.cit.aet.usermanagement.dto.LoginRequestDTO;
-import de.tum.cit.aet.usermanagement.dto.LoginResponseDTO;
 import de.tum.cit.aet.usermanagement.service.KeycloakAuthenticationService;
+import jakarta.servlet.http.HttpServletResponse;
+import java.time.Duration;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,8 +21,19 @@ public class EmailLoginResource {
     private final KeycloakAuthenticationService keycloakAuthenticationService;
 
     @PostMapping("/login")
-    public ResponseEntity<LoginResponseDTO> login(@RequestBody LoginRequestDTO loginRequest) {
+    public ResponseEntity<Void> login(@RequestBody LoginRequestDTO loginRequest, HttpServletResponse response) {
         String token = keycloakAuthenticationService.loginWithCredentials(loginRequest.email(), loginRequest.password());
-        return ResponseEntity.ok(new LoginResponseDTO(token));
+
+        ResponseCookie cookie = ResponseCookie.from("access_token", token)
+            .httpOnly(true)
+            .secure(true)
+            .sameSite("Strict")
+            .path("/")
+            .maxAge(Duration.ofHours(1))
+            .build();
+
+        response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
+
+        return ResponseEntity.ok().build();
     }
 }
