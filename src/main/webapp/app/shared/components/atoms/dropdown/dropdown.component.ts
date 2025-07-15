de9081b2d5970f32efbabad3562df3fe03b466ee
Ishani Budhwar <ge87tev@mytum.de>
2025-07-15 15:49:14 +0200

    add error border in dropdowns for validation

--- src/main/webapp/app/shared/components/atoms/dropdown/dropdown.component.ts
+++ src/main/webapp/app/shared/components/atoms/dropdown/dropdown.component.ts
@@ -1,11 +1,11 @@
-import { Component, ViewEncapsulation, input, output } from '@angular/core';
+import { Component, ViewEncapsulation, computed, input, output, signal } from '@angular/core';
 import { DropdownModule } from 'primeng/dropdown';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { TooltipModule } from 'primeng/tooltip';
 
-import { TranslateDirective } from '../../../language';
+import SharedModule from '../../../shared.module';
 
 export type DropdownOption = {
   name: string;
@@ -18,7 +18,7 @@ export type DropdownOption = {
   standalone: true,
   templateUrl: './dropdown.component.html',
   styleUrls: ['./dropdown.component.scss'],
-  imports: [DropdownModule, FontAwesomeModule, FormsModule, CommonModule, TooltipModule, TranslateDirective],
+  imports: [DropdownModule, FontAwesomeModule, FormsModule, CommonModule, TooltipModule, SharedModule],
   encapsulation: ViewEncapsulation.None,
 })
 export class DropdownComponent {
@@ -35,13 +35,36 @@ export class DropdownComponent {
   width = input<string>('100%');
   icon = input<string | undefined>(undefined);
   tooltipText = input<string | undefined>(undefined);
-  translateItems = input<boolean>(false);
+  shouldTranslate = input<boolean>(false);
+  displayErrorText = input<boolean>(true); // Whether to add padding to display error text (disable where not necessary: e.g. sort bar dropdown)
 
   selectedChange = output<DropdownOption>();
 
   isOpen = false;
+  isTouched = signal(false);
+
+  // Computed property that determines if the field is invalid
+  isInvalid = computed(() => {
+    return this.required() && this.isTouched() && !this.selected();
+  });
+
+  inputState = computed(() => {
+    if (!this.isTouched()) return 'untouched';
+    if (this.isInvalid()) return 'invalid';
+    return 'valid';
+  });
+
+  // Error message for required validation
+  errorMessage = computed<string | null>(() => {
+    return 'global.input.error.required';
+  });
 
   onSelectionChange(value: DropdownOption): void {
     this.selectedChange.emit(value);
+  }
+
+  onBlur(): void {
+    this.isTouched.set(true);
+    console.log('test');
   }
 }
