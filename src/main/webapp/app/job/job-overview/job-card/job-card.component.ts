2d5aaac4d8f412da0c442d76ea24fa3789fda5ef
Ishani Budhwar <ge87tev@mytum.de>
2025-07-15 23:36:06 +0200

    convert workload from hour/week to percentage and add translation directly to job card

--- src/main/webapp/app/job/job-overview/job-card/job-card.component.ts
+++ src/main/webapp/app/job/job-overview/job-card/job-card.component.ts
@@ -5,6 +5,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { CommonModule } from '@angular/common';
 import dayjs from 'dayjs/esm';
 
+import SharedModule from '../../../shared/shared.module';
 import { ButtonComponent } from '../../../shared/components/atoms/button/button.component';
 
 @Component({
@@ -12,25 +13,26 @@ import { ButtonComponent } from '../../../shared/components/atoms/button/button.
   templateUrl: './job-card.component.html',
   styleUrls: ['./job-card.component.scss'],
   standalone: true,
-  imports: [FontAwesomeModule, CardModule, ButtonComponent, CommonModule],
+  imports: [FontAwesomeModule, CardModule, ButtonComponent, CommonModule, SharedModule],
 })
 export class JobCardComponent {
   jobId = input<string>('');
   jobTitle = input<string>('');
-  fieldOfStudies = input<string>('');
-  location = input<string>('');
-  professor = input<string>('');
-  workload = input<string>('');
-  startDate = input<string>('');
+  fieldOfStudies = input<string | undefined>('');
+  location = input<string | undefined>('');
+  professor = input<string | undefined>('');
+  workload = input<number | undefined>(undefined);
+  startDate = input<string | undefined>('');
   relativeTime = input<string>('');
   // TO-DO: Replace value of headerColor with a color corresponding to the field of study
   headerColor = input<string>('var(--p-secondary-color)');
   // TO-DO: Replace value of icon with an icon corresponding to the field of study
   icon = input<string>('flask-vial');
 
-  readonly formattedStartDate = computed(() =>
-    this.startDate() ? dayjs(this.startDate()).format('DD.MM.YYYY') : 'No Start Date specified',
-  );
+  readonly formattedStartDate = computed(() => (this.startDate() !== undefined ? dayjs(this.startDate()).format('DD.MM.YYYY') : undefined));
+
+  // Convert workload from hours/week to percentage
+  readonly formattedWorkload = computed(() => (this.workload() !== undefined ? (this.workload()! / 40) * 100 : undefined));
 
   private router = inject(Router);
 
