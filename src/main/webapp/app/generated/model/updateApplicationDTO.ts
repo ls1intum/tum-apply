70e86a7b5fb2ae77343f90f3cb4472bf5cbcd239
Stephan Krusche <krusche@tum.de>
2025-07-24 13:03:25 +0200

    `Development`: Update server dependencies (#302)
    * update server dependencies
    
    * fix Tests
    
    * adapt openapi
    
    * fix mysql
    
    * fix merge conflict
    
    ---------
    
    Co-authored-by: Moritz <moritzschmidt1@gmail.com>
    Co-authored-by: Marc Fett <marc.fett@web.de>
    Co-authored-by: aniruddhzaveri <aniruddh.zaveri@tum.de>

--- src/main/webapp/app/generated/model/updateApplicationDTO.ts
+++ src/main/webapp/app/generated/model/updateApplicationDTO.ts
@@ -20,16 +20,16 @@ export interface UpdateApplicationDTO {
     motivation?: string;
 }
 export namespace UpdateApplicationDTO {
-    export type ApplicationStateEnum = 'SAVED' | 'SENT' | 'ACCEPTED' | 'IN_REVIEW' | 'REJECTED' | 'WITHDRAWN' | 'JOB_CLOSED';
     export const ApplicationStateEnum = {
-        Saved: 'SAVED' as ApplicationStateEnum,
-        Sent: 'SENT' as ApplicationStateEnum,
-        Accepted: 'ACCEPTED' as ApplicationStateEnum,
-        InReview: 'IN_REVIEW' as ApplicationStateEnum,
-        Rejected: 'REJECTED' as ApplicationStateEnum,
-        Withdrawn: 'WITHDRAWN' as ApplicationStateEnum,
-        JobClosed: 'JOB_CLOSED' as ApplicationStateEnum
-    };
+        Saved: 'SAVED',
+        Sent: 'SENT',
+        Accepted: 'ACCEPTED',
+        InReview: 'IN_REVIEW',
+        Rejected: 'REJECTED',
+        Withdrawn: 'WITHDRAWN',
+        JobClosed: 'JOB_CLOSED'
+    } as const;
+    export type ApplicationStateEnum = typeof ApplicationStateEnum[keyof typeof ApplicationStateEnum];
 }
 
 
