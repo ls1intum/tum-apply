bb9a274c5f103767f28aa61a83bb030cc47ea98e
Marc Fett <marc.fett@web.de>
2025-07-27 23:17:17 +0200

    prettier

--- src/main/java/de/tum/cit/aet/core/config/SecurityConfiguration.java
+++ src/main/java/de/tum/cit/aet/core/config/SecurityConfiguration.java
@@ -37,85 +37,85 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                // Disables CSRF (Cross-Site Request Forgery) protection; useful in stateless APIs where the token management is unnecessary.
-                .csrf(CsrfConfigurer::disable)
-                // Adds a CORS (Cross-Origin Resource Sharing) filter before the username/password authentication to handle cross-origin requests.
-                .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
-                // Adds a custom filter for Single Page Applications (SPA), i.e. the client, after the basic authentication filter.
-                .addFilterAfter(new SpaWebFilter(), BasicAuthenticationFilter.class)
-                // Configures security headers.
-                .headers(headers ->
-                        headers
-                                // Sets Content Security Policy (CSP) directives to prevent XSS attacks.
-                                .contentSecurityPolicy(csp -> csp.policyDirectives("script-src 'self' 'unsafe-inline' 'unsafe-eval'"))
-                                // Prevents the website from being framed, avoiding clickjacking attacks.
-                                .frameOptions(HeadersConfigurer.FrameOptionsConfig::disable) // Sets Referrer Policy to limit the amount of referrer information sent with requests.
-                                .referrerPolicy(referrer -> referrer.policy(ReferrerPolicyHeaderWriter.ReferrerPolicy.STRICT_ORIGIN_WHEN_CROSS_ORIGIN))
-                                // Disables HTTP Strict Transport Security as it is managed at the reverse proxy level (typically nginx).
-                                .httpStrictTransportSecurity((HeadersConfigurer.HstsConfig::disable))
-                                // Defines Permissions Policy to restrict what features the browser is allowed to use.
-                                .permissionsPolicyHeader(permissions ->
-                                        permissions.policy(
-                                                "camera=(), fullscreen=(*), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), midi=(), payment=(), sync-xhr=()"
-                                        )
-                                )
-                )
-                // Configures sessions to be stateless; appropriate for REST APIs where no session is required.
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                // Configures authorization for various URL patterns. The patterns are considered in order.
-                .authorizeHttpRequests(requests ->
-                        requests
-                                // NOTE: Always have a look at {@link de.tum.cit.aet.artemis.core.security.filter.SpaWebFilter} to see which URLs are forwarded to the SPA
-                                // Client related URLs and publicly accessible information (allowed for everyone).
-                                .requestMatchers("/", "/index.html", "/public/**")
-                                .permitAll()
-                                .requestMatchers("/*.js", "/*.css", "/*.map", "/*.json")
-                                .permitAll()
-                                .requestMatchers("/manifest.webapp", "/robots.txt")
-                                .permitAll()
-                                .requestMatchers("/assets/**")
-                                .permitAll()
-                                .requestMatchers("/content/**", "/i18n/*.json", "/logo/*")
-                                .permitAll()
-                                .requestMatchers("/media/**")
-                                .permitAll()
-                                .requestMatchers("/favicon.ico")
-                                .permitAll()
-                                // Information and health endpoints do not need authentication
-                                .requestMatchers("/management/info", "/management/health")
-                                .permitAll()
-                                // Admin area requires specific authority.
-                                .requestMatchers("/api/*/admin/**")
-                                .hasAuthority("ADMIN")
-                                // Publicly accessible API endpoints (allowed for everyone).
-                                .requestMatchers("/api/*/public/**")
-                                .permitAll()
-                                .requestMatchers("/api/jobs/available", "/api/jobs/available/**", "/api/jobs/detail/**")
-                                .permitAll()
-                                .requestMatchers("/api/auth/login")
-                                .permitAll()
-                                .requestMatchers("/api/**")
-                                .authenticated()
-                                .requestMatchers("/login/webauthn")
-                                .permitAll()
-                                // Websocket and other specific endpoints allowed without authentication.
-                                .requestMatchers("/websocket/**")
-                                .permitAll()
-                                .requestMatchers("/.well-known/jwks.json")
-                                .permitAll()
-                                .requestMatchers("/.well-known/assetlinks.json")
-                                .permitAll()
-                                // Prometheus endpoint protected by IP address.
-                                .requestMatchers("/management/prometheus/**")
-                                .permitAll()
-                                .requestMatchers(("/api-docs"))
-                                .permitAll()
-                                .requestMatchers(("/api-docs.yaml"))
-                                .permitAll()
-                                .requestMatchers("/swagger-ui/**")
-                                .permitAll()
-                )
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(customJwtAuthenticationConverter)));
+            // Disables CSRF (Cross-Site Request Forgery) protection; useful in stateless APIs where the token management is unnecessary.
+            .csrf(CsrfConfigurer::disable)
+            // Adds a CORS (Cross-Origin Resource Sharing) filter before the username/password authentication to handle cross-origin requests.
+            .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
+            // Adds a custom filter for Single Page Applications (SPA), i.e. the client, after the basic authentication filter.
+            .addFilterAfter(new SpaWebFilter(), BasicAuthenticationFilter.class)
+            // Configures security headers.
+            .headers(headers ->
+                headers
+                    // Sets Content Security Policy (CSP) directives to prevent XSS attacks.
+                    .contentSecurityPolicy(csp -> csp.policyDirectives("script-src 'self' 'unsafe-inline' 'unsafe-eval'"))
+                    // Prevents the website from being framed, avoiding clickjacking attacks.
+                    .frameOptions(HeadersConfigurer.FrameOptionsConfig::disable) // Sets Referrer Policy to limit the amount of referrer information sent with requests.
+                    .referrerPolicy(referrer -> referrer.policy(ReferrerPolicyHeaderWriter.ReferrerPolicy.STRICT_ORIGIN_WHEN_CROSS_ORIGIN))
+                    // Disables HTTP Strict Transport Security as it is managed at the reverse proxy level (typically nginx).
+                    .httpStrictTransportSecurity((HeadersConfigurer.HstsConfig::disable))
+                    // Defines Permissions Policy to restrict what features the browser is allowed to use.
+                    .permissionsPolicyHeader(permissions ->
+                        permissions.policy(
+                            "camera=(), fullscreen=(*), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), midi=(), payment=(), sync-xhr=()"
+                        )
+                    )
+            )
+            // Configures sessions to be stateless; appropriate for REST APIs where no session is required.
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            // Configures authorization for various URL patterns. The patterns are considered in order.
+            .authorizeHttpRequests(requests ->
+                requests
+                    // NOTE: Always have a look at {@link de.tum.cit.aet.artemis.core.security.filter.SpaWebFilter} to see which URLs are forwarded to the SPA
+                    // Client related URLs and publicly accessible information (allowed for everyone).
+                    .requestMatchers("/", "/index.html", "/public/**")
+                    .permitAll()
+                    .requestMatchers("/*.js", "/*.css", "/*.map", "/*.json")
+                    .permitAll()
+                    .requestMatchers("/manifest.webapp", "/robots.txt")
+                    .permitAll()
+                    .requestMatchers("/assets/**")
+                    .permitAll()
+                    .requestMatchers("/content/**", "/i18n/*.json", "/logo/*")
+                    .permitAll()
+                    .requestMatchers("/media/**")
+                    .permitAll()
+                    .requestMatchers("/favicon.ico")
+                    .permitAll()
+                    // Information and health endpoints do not need authentication
+                    .requestMatchers("/management/info", "/management/health")
+                    .permitAll()
+                    // Admin area requires specific authority.
+                    .requestMatchers("/api/*/admin/**")
+                    .hasAuthority("ADMIN")
+                    // Publicly accessible API endpoints (allowed for everyone).
+                    .requestMatchers("/api/*/public/**")
+                    .permitAll()
+                    .requestMatchers("/api/jobs/available", "/api/jobs/available/**", "/api/jobs/detail/**")
+                    .permitAll()
+                    .requestMatchers("/api/auth/login")
+                    .permitAll()
+                    .requestMatchers("/api/**")
+                    .authenticated()
+                    .requestMatchers("/login/webauthn")
+                    .permitAll()
+                    // Websocket and other specific endpoints allowed without authentication.
+                    .requestMatchers("/websocket/**")
+                    .permitAll()
+                    .requestMatchers("/.well-known/jwks.json")
+                    .permitAll()
+                    .requestMatchers("/.well-known/assetlinks.json")
+                    .permitAll()
+                    // Prometheus endpoint protected by IP address.
+                    .requestMatchers("/management/prometheus/**")
+                    .permitAll()
+                    .requestMatchers(("/api-docs"))
+                    .permitAll()
+                    .requestMatchers(("/api-docs.yaml"))
+                    .permitAll()
+                    .requestMatchers("/swagger-ui/**")
+                    .permitAll()
+            )
+            .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(customJwtAuthenticationConverter)));
         return http.build();
     }
 }
