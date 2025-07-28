cedb4095eed3b748d04ab6ff617a942bf2a1451d
Moritz <moritzschmidt1@gmail.com>
2025-07-28 13:59:54 +0200

    add dynamic table to research-group-templates.ts

--- src/main/webapp/app/usermanagement/research-group/research-group-templates/research-group-templates.ts
+++ src/main/webapp/app/usermanagement/research-group/research-group-templates/research-group-templates.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+
+import { DynamicTableComponent } from '../../../shared/components/organisms/dynamic-table/dynamic-table.component';
+import { EmailTemplateResourceService } from '../../../generated';
 
 @Component({
   selector: 'jhi-research-group-templates',
-  imports: [],
+  imports: [DynamicTableComponent],
   templateUrl: './research-group-templates.html',
   styleUrl: './research-group-templates.scss',
 })
-export class ResearchGroupTemplates {}
+export class ResearchGroupTemplates {
+  protected pageNumber = signal<number>(0);
+  protected pageSize = signal<number>(10);
+
+  protected emailTemplateService = inject(EmailTemplateResourceService);
+
+  protected readonly columns = [
+    { field: 'templateName', header: 'templateName', width: '26rem' },
+    { field: 'createdBy', header: 'createdBy', width: '10rem' },
+    { field: '', header: '', width: '5rem' },
+    { field: '', header: '', width: '5rem' },
+  ];
+}
