919dba45c7fd1563805c192350d9240b564e74d0
Marc Fett <marc.fett@web.de>
2025-08-03 01:05:32 +0200

    change to own db for clearflask

--- src/main/docker/clearflask/mysql/clearflask-init.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE IF NOT EXISTS clearflask CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-CREATE USER IF NOT EXISTS 'clearflask'@'%' IDENTIFIED BY 'clearflask';
-GRANT ALL PRIVILEGES ON clearflask.* TO 'clearflask'@'%';
-FLUSH PRIVILEGES;
