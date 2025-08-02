c477e6a00e6cb1abd9acba9f964082f983afcfd8
Marc Fett <marc.fett@web.de>
2025-08-02 12:06:32 +0200

    fix tests

--- src/main/webapp/app/shared/components/atoms/password-input/password-input.spec.ts
+++ src/main/webapp/app/shared/components/atoms/password-input/password-input.spec.ts
@@ -1,14 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  MissingTranslationHandler,
+  TranslateCompiler,
+  TranslateLoader,
+  TranslateModule,
+  TranslateParser,
+  TranslateService,
+  TranslateStore,
+} from '@ngx-translate/core';
 
 import { PasswordInputComponent } from './password-input';
 
-describe('PasswordInput', () => {
+describe('PasswordInputComponent', () => {
   let component: PasswordInputComponent;
   let fixture: ComponentFixture<PasswordInputComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PasswordInputComponent],
+      imports: [PasswordInputComponent, TranslateModule.forRoot()],
+      providers: [
+        TranslateStore,
+        TranslateLoader,
+        TranslateCompiler,
+        TranslateParser,
+        {
+          provide: MissingTranslationHandler,
+          useValue: { handle: jest.fn() },
+        },
+        TranslateService,
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(PasswordInputComponent);
