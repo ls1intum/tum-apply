c2343f7095a81b4b5a59c53827056353e7b8e069
Marc Fett <marc.fett@web.de>
2025-08-03 15:19:48 +0200

    openapi

--- src/main/webapp/app/generated/api/api.ts
+++ src/main/webapp/app/generated/api/api.ts
@@ -1,22 +1,16 @@
 export * from './applicationEvaluationResource.service';
 import { ApplicationEvaluationResourceService } from './applicationEvaluationResource.service';
-
-export * from './applicationResource.service';
 import { ApplicationResourceService } from './applicationResource.service';
-
-export * from './authenticationResource.service';
 import { AuthenticationResourceService } from './authenticationResource.service';
-
-export * from './documentResource.service';
 import { DocumentResourceService } from './documentResource.service';
-
-export * from './emailSettingResource.service';
 import { EmailSettingResourceService } from './emailSettingResource.service';
-
-export * from './jobResource.service';
 import { JobResourceService } from './jobResource.service';
-
-export * from './userResource.service';
 import { UserResourceService } from './userResource.service';
 
+export * from './applicationResource.service';
+export * from './authenticationResource.service';
+export * from './documentResource.service';
+export * from './emailSettingResource.service';
+export * from './jobResource.service';
+export * from './userResource.service';
 export const APIS = [ApplicationEvaluationResourceService, ApplicationResourceService, AuthenticationResourceService, DocumentResourceService, EmailSettingResourceService, JobResourceService, UserResourceService];
