d9d8cfbba8d30b39327354da6fadeac88247a87b
Kiara Copony <ge65min@mytum.de>
2025-08-08 18:21:35 +0200

    create template for confirm dialog

---
+++ src/main/webapp/app/shared/components/atoms/confirm-dialog/confirm-dialog.ts
@@ -0,0 +1,36 @@
+import { Component, ViewEncapsulation, inject, input, output } from '@angular/core';
+import { ConfirmationService } from 'primeng/api';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+import { ButtonComponent } from '../button/button.component';
+
+@Component({
+  selector: 'jhi-confirm-dialog',
+  templateUrl: './confirm-dialog.html',
+  styleUrl: './confirm-dialog.scss',
+  providers: [ConfirmationService],
+  imports: [ConfirmDialogModule, ButtonComponent, FontAwesomeModule],
+  encapsulation: ViewEncapsulation.None,
+})
+export class ConfirmDialog {
+  label = input<string | undefined>(undefined);
+  header = input<string | undefined>(undefined);
+  message = input<string | undefined>(undefined);
+
+  data = input<string | undefined>(undefined);
+
+  confirmed = output<unknown>();
+
+  private confirmationService = inject(ConfirmationService);
+
+  confirm(): void {
+    this.confirmationService.confirm({
+      message: this.message(),
+      header: this.header(),
+      accept: () => {
+        this.confirmed.emit(this.data());
+      },
+    });
+  }
+}
