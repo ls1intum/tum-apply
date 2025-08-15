7bc594fd0a4e7c5b872c327b37622ae76c335da6
Moritz <moritzschmidt1@gmail.com>
2025-08-15 15:00:07 +0200

    add adjustable width to tag component

--- src/main/webapp/app/shared/components/atoms/tag/tag.component.ts
+++ src/main/webapp/app/shared/components/atoms/tag/tag.component.ts
@@ -1,10 +1,11 @@
 import { Component, computed, input } from '@angular/core';
 import { Tag } from 'primeng/tag';
+import { NgStyle } from '@angular/common';
 import { FontAwesomeModule, IconDefinition } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'jhi-tag',
-  imports: [Tag, FontAwesomeModule],
+  imports: [Tag, FontAwesomeModule, NgStyle],
   templateUrl: './tag.component.html',
   styleUrl: './tag.component.scss',
   standalone: true,
@@ -15,6 +16,16 @@ export class TagComponent {
   icon = input<IconDefinition | undefined>(undefined);
   round = input<boolean>(false);
   iconRight = input<boolean>(false);
+  width = input<string | undefined>(undefined);
 
   readonly iconProp = computed(() => this.icon() as IconDefinition);
+
+  widthStyle = computed(() => {
+    if (this.width() != null) {
+      return {
+        width: this.width(),
+      };
+    }
+    return undefined;
+  });
 }
