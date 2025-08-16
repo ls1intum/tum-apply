27a5babe1cb98db45b4f5513b100fda696487539
Kiara Copony <ge65min@mytum.de>
2025-08-16 15:49:24 +0200

    create toasts globally instead of in each component

--- src/main/webapp/app/app.config.ts
+++ src/main/webapp/app/app.config.ts
@@ -17,6 +17,7 @@ import { ScrollingModule } from '@angular/cdk/scrolling';
 import { DatePipe } from '@angular/common';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { providePrimeNG } from 'primeng/config';
+import { MessageService } from 'primeng/api';
 
 import { TUMApplyPreset } from '../content/theming/tumapplypreset';
 
@@ -48,6 +49,7 @@ export function apiConfigFactory(): Configuration {
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    MessageService,
     provideAppInitializer(initializeAuth),
     provideZonelessChangeDetection(),
     provideRouter(routes, withRouterConfig({ onSameUrlNavigation: 'reload' })),
