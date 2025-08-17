ed1c9e47347efe0498ce2284a5c3e25975417248
Marc Fett <marc.fett@web.de>
2025-08-17 21:03:35 +0200

    fix error in main

--- src/main/webapp/app/layouts/main/main.component.ts
+++ src/main/webapp/app/layouts/main/main.component.ts
@@ -1,7 +1,8 @@
-import { Component, Renderer2, RendererFactory2, inject } from '@angular/core';
+import { Component, Renderer2, RendererFactory2, computed, inject } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
 import dayjs from 'dayjs/esm';
+import { AccountService } from 'app/core/auth/account.service';
 import { AppPageTitleStrategy } from 'app/app-page-title-strategy';
 import { SidebarComponent } from 'app/shared/components/organisms/sidebar/sidebar.component';
 
@@ -17,6 +18,10 @@ import { HeaderComponent } from '../../shared/components/organisms/header/header
   imports: [HeaderComponent, RouterOutlet, SidebarComponent, FooterComponent, PageRibbonComponent],
 })
 export default class MainComponent {
+  readonly accountService = inject(AccountService);
+  loggedIn = computed(() => {
+    return this.accountService.signedIn();
+  });
   private readonly router = inject(Router);
   private readonly renderer: Renderer2;
   private readonly appPageTitleStrategy = inject(AppPageTitleStrategy);
