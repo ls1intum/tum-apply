2d596a63d91151a087f3a44df68bec68e213c1a2
Kiara Copony <ge65min@mytum.de>
2025-08-17 21:29:13 +0200

    `Usability`: Create Confirmation dialogs (#525)
    * create template for confirm dialog
    
    * add severity variable
    
    * use global string for cancel title
    
    * add variant attribut for overview button
    
    * increase max width for confirm dialog
    
    * integrade confirm dialog for withdraw button on applicant application overview
    
    * exchange withdrawing with deleting on deleteapplication call
    
    * integrate confirm dialog to delete operation in applicant application overview
    
    * add boolean showOpenButton to confirm dialog to evaluate if open Button is necessary
    
    * add shouldTranslate attribut to all buttons in confirm-dialog template
    
    * remove shouldTranslate
    
    * integrate confirm dialog to application creation send button
    
    * integrate confirm dialog to close action in my position page
    
    * remove todo from close method
    
    * rename json strings for close action in my position page
    
    * integrate dialog to delete option in my position page
    
    * change english submit text to make it clearer
    
    * integrate confirm dialog to publish button in job creation form
    
    * add icon attribute to confirm button
    
    * add ban icon to cancel button in confirm dialog
    
    * add variant to confirm button
    
    * integrate icons to all confirm dialogs
    
    * add trash icon to delete button in application overview
    
    * add icons to my position page spec file
    
    * add confirm dialogs to job-detail buttons
    
    * move styling from html to scss
    
    * use gender neutral language in withdraw Message

--- src/main/webapp/app/application/application-overview-for-applicant/application-overview-for-applicant.component.ts
+++ src/main/webapp/app/application/application-overview-for-applicant/application-overview-for-applicant.component.ts
@@ -12,6 +12,8 @@ import SharedModule from 'app/shared/shared.module';
 import { AccountService } from 'app/core/auth/account.service';
 import { TranslateModule } from '@ngx-translate/core';
 import { TranslateService } from '@ngx-translate/core';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ConfirmDialog } from 'app/shared/components/atoms/confirm-dialog/confirm-dialog';
 
 import { ApplicationStateForApplicantsComponent } from '../application-state-for-applicants/application-state-for-applicants.component';
 
@@ -26,6 +28,8 @@ import { ApplicationStateForApplicantsComponent } from '../application-state-for
     ToastComponent,
     ApplicationStateForApplicantsComponent,
     RouterModule,
+    ConfirmDialogModule,
+    ConfirmDialog,
   ],
   templateUrl: './application-overview-for-applicant.component.html',
   styleUrl: './application-overview-for-applicant.component.scss',
@@ -157,7 +161,7 @@ export default class ApplicationOverviewForApplicantComponent {
         if (event) this.loadPage(event);
       },
       error: err => {
-        this.toastService.showError({ detail: 'Error withdrawing the application' });
+        this.toastService.showError({ detail: 'Error deleting the application' });
         console.error('Delete failed', err);
       },
     });
@@ -165,20 +169,16 @@ export default class ApplicationOverviewForApplicantComponent {
   }
 
   onWithdrawApplication(applicationId: string): void {
-    // TODO nicer looking confirm
-    const confirmWithdraw = confirm('Do you really want to withdraw this application?');
-    if (confirmWithdraw) {
-      this.applicationService.withdrawApplication(applicationId).subscribe({
-        next: () => {
-          this.toastService.showSuccess({ detail: 'Application successfully withdrawn' });
-          const event = this.lastLazyLoadEvent();
-          if (event) this.loadPage(event);
-        },
-        error: err => {
-          this.toastService.showError({ detail: 'Error withdrawing the application' });
-          console.error('Withdraw failed', err);
-        },
-      });
-    }
+    this.applicationService.withdrawApplication(applicationId).subscribe({
+      next: () => {
+        this.toastService.showSuccess({ detail: 'Application successfully withdrawn' });
+        const event = this.lastLazyLoadEvent();
+        if (event) this.loadPage(event);
+      },
+      error: err => {
+        this.toastService.showError({ detail: 'Error withdrawing the application' });
+        console.error('Withdraw failed', err);
+      },
+    });
   }
 }
