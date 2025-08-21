1af2a7629a16e78c5a42344f91e45aab9acaf1e7
Bofan Zhu <bofan.zhu@tum.de>
2025-08-21 22:31:43 +0200

    remove non-disclosure for optional field

--- src/main/webapp/app/application/application-creation/application-creation-page1/application-creation-page1.component.ts
+++ src/main/webapp/app/application/application-creation/application-creation-page1/application-creation-page1.component.ts
@@ -33,7 +33,6 @@ export const selectGender: SelectOption[] = [
   { value: 'female', name: 'Female' },
   { value: 'male', name: 'Male' },
   { value: 'other', name: 'Other' },
-  { value: 'prefer_not_to_say', name: 'Prefer not to say' },
 ];
 
 export const selectLanguage: SelectOption[] = [
