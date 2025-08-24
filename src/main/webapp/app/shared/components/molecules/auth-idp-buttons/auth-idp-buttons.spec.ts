d88b86d302eba4602f85a18f3263db0d2d81d604
Marc Fett <marc.fett@web.de>
2025-08-24 11:16:08 +0200

    add auth idp button component

---
+++ src/main/webapp/app/shared/components/molecules/auth-idp-buttons/auth-idp-buttons.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AuthIdpButtons } from './auth-idp-buttons';
+
+describe('AuthIdpButtons', () => {
+  let component: AuthIdpButtons;
+  let fixture: ComponentFixture<AuthIdpButtons>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AuthIdpButtons]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AuthIdpButtons);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
