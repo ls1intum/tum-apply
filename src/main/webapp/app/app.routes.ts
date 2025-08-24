c74a8a7fb489c1e918e826426a3e3a1dce6f2e86
Kiara Copony <ge65min@mytum.de>
2025-08-24 22:31:19 +0200

    `General`: Fix browser tab title (#586)
    * remove title from app routes
    
    * change browser title to specific page

--- src/main/webapp/app/app.routes.ts
+++ src/main/webapp/app/app.routes.ts
@@ -160,14 +160,14 @@ const routes: Routes = [
     canActivate: [UserRouteAccessService],
     data: { authorities: [] },
     loadComponent: () => import('./shared/pages/imprint-page/imprint-page.component').then(m => m.ImprintPageComponent),
-    title: 'imprint',
+    title: 'footer.imprint',
   },
   {
     path: 'privacy',
     canActivate: [UserRouteAccessService],
     data: { authorities: [] },
     loadComponent: () => import('./shared/pages/privacy-page/privacy-page.component').then(m => m.PrivacyPageComponent),
-    title: 'privacy',
+    title: 'footer.privacy',
   },
 
   // ======================================================================================
@@ -178,7 +178,7 @@ const routes: Routes = [
     canActivate: [UserRouteAccessService],
     data: { authorities: [UserShortDTO.RolesEnum.Admin, UserShortDTO.RolesEnum.Professor, UserShortDTO.RolesEnum.Applicant] },
     loadComponent: () => import('./shared/settings/settings.component').then(m => m.SettingsComponent),
-    title: 'Settings',
+    title: 'settings.header',
   },
 
   // ======================================================================================
