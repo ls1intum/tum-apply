7e83c3db7f37863c8f132113c849088d7d67e8a3
Sarah Douglas <sarah.douglas@gmx.de>
2025-08-25 12:27:35 +0200

    fix: removed disabledEmail

--- src/main/webapp/app/application/application-creation/application-creation-page1/application-creation-page1.component.ts
+++ src/main/webapp/app/application/application-creation/application-creation-page1/application-creation-page1.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { Component, computed, effect, inject, input, model, output } from '@angular/core';
+import { Component, computed, effect, inject, model, output } from '@angular/core';
 import { ApplicationForApplicantDTO } from 'app/generated';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { DividerModule } from 'primeng/divider';
 import { TranslateModule } from '@ngx-translate/core';
 import SharedModule from 'app/shared/shared.module';
+import { AccountService } from 'app/core/auth/account.service';
 
 import { SelectComponent, SelectOption } from '../../../shared/components/atoms/select/select.component';
 import { DatePickerComponent } from '../../../shared/components/atoms/datepicker/datepicker.component';
@@ -78,14 +79,16 @@ export const getPage1FromApplication = (application: ApplicationForApplicantDTO)
 export default class ApplicationCreationPage1Component {
   data = model.required<ApplicationCreationPage1Data>();
 
-  disableEmail = input<boolean>(true);
-
   valid = output<boolean>();
   changed = output<boolean>();
+
+  disabledEmail = computed<boolean>(() => this.accountService.signedIn());
 
   selectGenderLocal = selectGender;
   selectLanguageLocal = selectLanguage;
   selectNationalityLocal = selectNationality;
+
+  accountService = inject(AccountService);
   fb = inject(FormBuilder);
   page1Form = computed(() => {
     const currentData = this.data();
