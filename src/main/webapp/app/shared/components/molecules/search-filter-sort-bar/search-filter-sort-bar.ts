34b1f961c9a724d684a3bae01fa8f0733d55ac3c
Kiara Copony <ge65min@mytum.de>
2025-09-01 22:52:21 +0200

    create design for search bar

--- src/main/webapp/app/shared/components/molecules/search-filter-sort-bar/search-filter-sort-bar.ts
+++ src/main/webapp/app/shared/components/molecules/search-filter-sort-bar/search-filter-sort-bar.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'jhi-search-filter-sort-bar',
-  imports: [],
+  imports: [FormsModule, InputTextModule, IconFieldModule, InputIconModule, FontAwesomeModule],
   templateUrl: './search-filter-sort-bar.html',
   styleUrl: './search-filter-sort-bar.scss',
 })
-export class SearchFilterSortBar {}
+export class SearchFilterSortBar {
+  searchText = input<string | undefined>(undefined);
+  inputText = '';
+
+  onSearch(): void {}
+}
