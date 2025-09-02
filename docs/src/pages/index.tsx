a4b9f4c0dcd0c1b6c28659771301553ca33658cb
Marc Fett <marc.fett@web.de>
2025-09-02 12:26:58 +0200

    adapt title

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -1,9 +1,7 @@
-import type {ReactNode} from 'react';
+import type { ReactNode } from 'react';
 import clsx from 'clsx';
 import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
 
 import styles from './index.module.css';
@@ -31,7 +29,7 @@ function HomepageHeader() {
 export default function Home(): ReactNode {
   return (
     <Layout
-      title={`TumApply Documentation`}
+      title={`TUMApply Documentation`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
     </Layout>
