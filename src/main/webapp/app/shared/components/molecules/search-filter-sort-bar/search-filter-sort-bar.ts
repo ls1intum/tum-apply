61513eb77cca74adcef2c950a15a0ef342ad85e9
Kiara Copony <ge65min@mytum.de>
2025-09-02 13:54:48 +0200

    display found entitites below search bar

--- src/main/webapp/app/shared/components/molecules/search-filter-sort-bar/search-filter-sort-bar.ts
+++ src/main/webapp/app/shared/components/molecules/search-filter-sort-bar/search-filter-sort-bar.ts
@@ -4,15 +4,26 @@ import { InputTextModule } from 'primeng/inputtext';
 import { IconFieldModule } from 'primeng/iconfield';
 import { InputIconModule } from 'primeng/inputicon';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { TranslateModule } from '@ngx-translate/core';
+
+import TranslateDirective from '../../../language/translate.directive';
 
 @Component({
   selector: 'jhi-search-filter-sort-bar',
-  imports: [FormsModule, InputTextModule, IconFieldModule, InputIconModule, FontAwesomeModule],
+  imports: [FormsModule, InputTextModule, IconFieldModule, InputIconModule, FontAwesomeModule, TranslateModule, TranslateDirective],
   templateUrl: './search-filter-sort-bar.html',
   styleUrl: './search-filter-sort-bar.scss',
 })
 export class SearchFilterSortBar {
+  // total number of records found
+  totalRecords = input<number>(0);
   searchText = input<string | undefined>(undefined);
+
+  // translation keys used for the total number of records found
+  // those fields should already be translated within the parent component
+  singleEntity = input.required<string>();
+  multipleEntities = input.required<string>();
+
   inputText = '';
 
   onSearch(): void {}
