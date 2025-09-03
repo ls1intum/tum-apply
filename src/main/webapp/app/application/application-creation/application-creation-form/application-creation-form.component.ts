77c52455b2eaf02061d6de4d2148237f4ee99366
Abi107717 <a.abinaya.sivaguru@gmail.com>
2025-09-03 19:12:44 +0200

    changed cancel to back

--- src/main/webapp/app/application/application-creation/application-creation-form/application-creation-form.component.ts
+++ src/main/webapp/app/application/application-creation/application-creation-form/application-creation-form.component.ts
@@ -149,7 +149,7 @@ export default class ApplicationCreationFormComponent {
               })();
             },
             disabled: false,
-            label: 'entity.applicationSteps.buttons.cancel',
+            label: 'entity.applicationSteps.buttons.back',
             changePanel: false,
             shouldTranslate: true,
           },
