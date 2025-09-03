4d367a4f08573e613f80694c764fbcc14c3b44e8
Kiara Copony <ge65min@mytum.de>
2025-09-03 10:13:27 +0200

    fix search filter sort bar tests

--- src/main/webapp/app/shared/components/molecules/search-filter-sort-bar/search-filter-sort-bar.spec.ts
+++ src/main/webapp/app/shared/components/molecules/search-filter-sort-bar/search-filter-sort-bar.spec.ts
@@ -1,4 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import {
+  MissingTranslationHandler,
+  TranslateCompiler,
+  TranslateLoader,
+  TranslateModule,
+  TranslateParser,
+  TranslateStore,
+} from '@ngx-translate/core';
+import { missingTranslationHandler } from 'app/config/translation.config';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
 
 import { SearchFilterSortBar } from './search-filter-sort-bar';
 
@@ -8,11 +19,27 @@ describe('SearchFilterSortBar', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SearchFilterSortBar],
+      imports: [
+        SearchFilterSortBar,
+        TranslateModule.forRoot({
+          missingTranslationHandler: {
+            provide: MissingTranslationHandler,
+            useFactory: missingTranslationHandler,
+          },
+        }),
+      ],
+      providers: [TranslateStore, TranslateLoader, TranslateCompiler, TranslateParser],
     }).compileComponents();
 
     fixture = TestBed.createComponent(SearchFilterSortBar);
     component = fixture.componentInstance;
+
+    fixture.componentRef.setInput('singleEntity', '');
+    fixture.componentRef.setInput('multipleEntities', '');
+
+    const library = TestBed.inject(FaIconLibrary);
+    library.addIcons(faSearch);
+
     fixture.detectChanges();
   });
 
