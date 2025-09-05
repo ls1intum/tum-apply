1dc1fe091d4e631d0363c964e4ab59abc6579e4b
Marc Fett <marc.fett@web.de>
2025-09-05 23:57:53 +0200

    fix unused

--- src/main/webapp/app/shared/components/molecules/credentials-group/credentials-group.component.ts
+++ src/main/webapp/app/shared/components/molecules/credentials-group/credentials-group.component.ts
@@ -36,7 +36,7 @@ import { AuthOrchestratorService } from '../../../auth/data-access/auth-orchestr
 export class CredentialsGroupComponent {
   authOrchestrator = inject(AuthOrchestratorService);
 
-  submitHandler = input.required<({ email, password, otp }: { email: string; password?: string; otp?: string }) => Promise<boolean>>();
+  submitHandler = input.required<(credentials: { email: string; password?: string; otp?: string }) => Promise<boolean>>();
   showPassword = input<boolean>(true);
   submitLabel = input<string>('auth.login.emailLogin.login');
 
