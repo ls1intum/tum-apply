12dc680be03b87038f67eed80055c5bf815a0a73
Marc Fett <marc.fett@web.de>
2025-09-06 11:07:13 +0200

    remove args

--- src/main/webapp/app/shared/components/molecules/credentials-group/credentials-group.component.ts
+++ src/main/webapp/app/shared/components/molecules/credentials-group/credentials-group.component.ts
@@ -36,7 +36,7 @@ import { AuthOrchestratorService } from '../../../auth/data-access/auth-orchestr
 export class CredentialsGroupComponent {
   authOrchestrator = inject(AuthOrchestratorService);
 
-  submitHandler = input.required<(...args: [string, string?]) => Promise<boolean>>();
+  submitHandler = input.required<(..._: [string, string?]) => Promise<boolean>>();
   showPassword = input<boolean>(true);
   submitLabel = input<string>('auth.login.emailLogin.login');
 
