f07c32ec4e5958642f9f4d9069f96adb490e1bd8
Bofan Zhu <bofan.zhu@tum.de>
2025-09-06 14:49:10 +0200

    add missing test imports

--- src/main/webapp/app/usermanagement/research-group/research-group-members/research-group-members.component.spec.ts
+++ src/main/webapp/app/usermanagement/research-group/research-group-members/research-group-members.component.spec.ts
@@ -1,15 +1,48 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { TranslateModule } from '@ngx-translate/core';
+import { of } from 'rxjs';
+
+import { ResearchGroupResourceService } from '../../../generated/api/researchGroupResource.service';
+import { ToastService } from '../../../service/toast-service';
+import { AccountService } from '../../../core/auth/account.service';
 
 import { ResearchGroupMembersComponent } from './research-group-members.component';
+
+// Mock the services at the module level
+jest.mock('../../../generated/api/researchGroupResource.service');
+jest.mock('../../../service/toast-service');
+jest.mock('../../../core/auth/account.service');
 
 describe('ResearchGroupMembers', () => {
   let component: ResearchGroupMembersComponent;
   let fixture: ComponentFixture<ResearchGroupMembersComponent>;
 
   beforeEach(async () => {
+    // Mock the service methods
+    const mockResearchGroupService = {
+      getResearchGroupMembers: jest.fn().mockReturnValue(of([])),
+    };
+    const mockToastService = {
+      showError: jest.fn(),
+      showInfo: jest.fn(),
+    };
+    const mockAccountService = {
+      userId: 'test-user-id',
+    };
+
     await TestBed.configureTestingModule({
-      imports: [ResearchGroupMembersComponent],
+      imports: [ResearchGroupMembersComponent, TranslateModule.forRoot(), FontAwesomeModule],
+      providers: [
+        { provide: ResearchGroupResourceService, useValue: mockResearchGroupService },
+        { provide: ToastService, useValue: mockToastService },
+        { provide: AccountService, useValue: mockAccountService },
+      ],
     }).compileComponents();
+
+    const library = TestBed.inject(FaIconLibrary);
+    library.addIconPacks(fas);
 
     fixture = TestBed.createComponent(ResearchGroupMembersComponent);
     component = fixture.componentInstance;
