7c22dde681d8139b3e30d01ed59bb4b7e526c06b
Abi107717 <a.abinaya.sivaguru@gmail.com>
2025-09-08 12:38:15 +0200

    adjust toasts in application-overview-for-applicant.component.ts

--- src/main/webapp/app/application/application-overview-for-applicant/application-overview-for-applicant.component.ts
+++ src/main/webapp/app/application/application-overview-for-applicant/application-overview-for-applicant.component.ts
@@ -154,27 +154,26 @@ export default class ApplicationOverviewForApplicantComponent {
     // if (confirmDelete) {
     this.applicationService.deleteApplication(applicationId).subscribe({
       next: () => {
-        this.toastService.showSuccess({ detail: 'Application successfully deleted' });
+        this.toastService.showSuccessKey('entity.toast.applicationOverview.delete.success');
         const event = this.lastLazyLoadEvent();
         if (event) this.loadPage(event);
       },
       error: err => {
-        this.toastService.showError({ detail: 'Error deleting the application' });
+        this.toastService.showErrorKey('entity.toast.applicationOverview.delete.error');
         console.error('Delete failed', err);
       },
     });
-    // }
   }
 
   onWithdrawApplication(applicationId: string): void {
     this.applicationService.withdrawApplication(applicationId).subscribe({
       next: () => {
-        this.toastService.showSuccess({ detail: 'Application successfully withdrawn' });
+        this.toastService.showSuccessKey('entity.toast.applicationOverview.withdraw.success');
         const event = this.lastLazyLoadEvent();
         if (event) this.loadPage(event);
       },
       error: err => {
-        this.toastService.showError({ detail: 'Error withdrawing the application' });
+        this.toastService.showErrorKey('entity.toast.applicationOverview.withdraw.error');
         console.error('Withdraw failed', err);
       },
     });
