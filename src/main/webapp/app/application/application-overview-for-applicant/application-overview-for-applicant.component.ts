74083030563d2a0795b8a937084df32d16245fc0
Abi107717 <a.abinaya.sivaguru@gmail.com>
2025-09-09 12:05:55 +0200

    redo taosts in application overview

--- src/main/webapp/app/application/application-overview-for-applicant/application-overview-for-applicant.component.ts
+++ src/main/webapp/app/application/application-overview-for-applicant/application-overview-for-applicant.component.ts
@@ -173,12 +173,12 @@ export default class ApplicationOverviewForApplicantComponent {
   onWithdrawApplication(applicationId: string): void {
     this.applicationService.withdrawApplication(applicationId).subscribe({
       next: () => {
-        this.toastService.showSuccessKey('entity.toast.applicationOverview.withdraw.success');
+        this.toastService.showSuccess({ detail: 'Application successfully withdrawn' });
         const event = this.lastLazyLoadEvent();
         if (event) this.loadPage(event);
       },
       error: err => {
-        this.toastService.showErrorKey('entity.toast.applicationOverview.withdraw.error');
+        this.toastService.showError({ detail: 'Error withdrawing the application' });
         console.error('Withdraw failed', err);
       },
     });
