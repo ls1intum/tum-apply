8fbeb50501c054f9df9d44c0627f33504ea28c1f
Kiara Copony <ge65min@mytum.de>
2025-09-09 12:55:29 +0200

    remove comment

--- src/main/java/de/tum/cit/aet/evaluation/repository/impl/ApplicationEvaluationRepositoryImpl.java
+++ src/main/java/de/tum/cit/aet/evaluation/repository/impl/ApplicationEvaluationRepositoryImpl.java
@@ -147,16 +147,14 @@ public class ApplicationEvaluationRepositoryImpl implements ApplicationEvaluatio
         Root<Application> root = query.from(Application.class);
         Join<Application, Job> jobJoin = root.join(Application_.JOB);
 
-        query.select(jobJoin.get(Job_.TITLE))
-                .distinct(true)
-                .where(cb.and(
-                        cb.equal(
-                                jobJoin.get(Job_.RESEARCH_GROUP).get(ResearchGroup_.RESEARCH_GROUP_ID),
-                                researchGroupId)
-                // root.get(Application_.STATE).in(VIEWABLE_STATES) // Nur Applications in
-                // sichtbaren States
-                ))
-                .orderBy(cb.asc(jobJoin.get(Job_.TITLE)));
+                query.select(jobJoin.get(Job_.TITLE))
+                                .distinct(true)
+                                .where(cb.and(
+                                                cb.equal(
+                                                                jobJoin.get(Job_.RESEARCH_GROUP)
+                                                                                .get(ResearchGroup_.RESEARCH_GROUP_ID),
+                                                                researchGroupId)))
+                                .orderBy(cb.asc(jobJoin.get(Job_.TITLE)));
 
         return em.createQuery(query).getResultList();
     }
