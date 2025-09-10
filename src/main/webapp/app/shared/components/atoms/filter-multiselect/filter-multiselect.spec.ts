8d2764ebca16a30ff08b260474e4cb5952081794
Kiara Copony <ge65min@mytum.de>
2025-09-10 13:21:15 +0200

    fix client tests

--- src/main/webapp/app/shared/components/atoms/filter-multiselect/filter-multiselect.spec.ts
+++ src/main/webapp/app/shared/components/atoms/filter-multiselect/filter-multiselect.spec.ts
@@ -1,4 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  MissingTranslationHandler,
+  TranslateCompiler,
+  TranslateLoader,
+  TranslateModule,
+  TranslateParser,
+  TranslateStore,
+} from '@ngx-translate/core';
+import { missingTranslationHandler } from 'app/config/translation.config';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faChevronDown, faSearch } from '@fortawesome/free-solid-svg-icons';
 
 import { FilterMultiselect } from './filter-multiselect';
 
@@ -8,11 +19,28 @@ describe('FilterMultiselect', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [FilterMultiselect],
+      imports: [
+        FilterMultiselect,
+        TranslateModule.forRoot({
+          missingTranslationHandler: {
+            provide: MissingTranslationHandler,
+            useFactory: missingTranslationHandler,
+          },
+        }),
+      ],
+      providers: [TranslateStore, TranslateLoader, TranslateCompiler, TranslateParser],
     }).compileComponents();
 
     fixture = TestBed.createComponent(FilterMultiselect);
     component = fixture.componentInstance;
+
+    fixture.componentRef.setInput('filterLabel', '');
+    fixture.componentRef.setInput('filterSearchPlaceholder', '');
+
+    const library = TestBed.inject(FaIconLibrary);
+    library.addIcons(faSearch);
+    library.addIcons(faChevronDown);
+
     fixture.detectChanges();
   });
 
