e32a6c38b8306e4dcde1a0e702285b7d00c3fa7a
Bofan Zhu <bofan.zhu@tum.de>
2025-09-11 18:43:48 +0200

    update date and language

--- src/main/webapp/app/application/application-creation/application-creation-page3/application-creation-page3.component.ts
+++ src/main/webapp/app/application/application-creation/application-creation-page3/application-creation-page3.component.ts
@@ -141,6 +141,10 @@ export default class ApplicationCreationPage3Component {
         desiredStartDate: $event ?? '',
       });
     }
+    // Also update the form control to keep it in sync
+    this.page3Form.patchValue({
+      desiredStartDate: $event ?? '',
+    });
     this.emitChanged();
   }
 }
