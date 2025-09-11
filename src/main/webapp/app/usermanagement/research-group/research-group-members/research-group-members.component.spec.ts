a4fc49579ca42b73f58664528a21fb5e12cc9d0f
Bofan Zhu <bofan.zhu@tum.de>
2025-09-11 20:05:02 +0200

    mock services

--- src/main/webapp/app/usermanagement/research-group/research-group-members/research-group-members.component.spec.ts
+++ src/main/webapp/app/usermanagement/research-group/research-group-members/research-group-members.component.spec.ts
@@ -1,4 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+
+import { ResearchGroupResourceService } from '../../../generated/api/researchGroupResource.service';
+import { ToastService } from '../../../service/toast-service';
+import { AccountService } from '../../../core/auth/account.service';
 
 import { ResearchGroupMembersComponent } from './research-group-members.component';
 
@@ -7,8 +12,24 @@ describe('ResearchGroupMembers', () => {
   let fixture: ComponentFixture<ResearchGroupMembersComponent>;
 
   beforeEach(async () => {
+    const mockResearchGroupService = {
+      getResearchGroupMembers: jest.fn().mockReturnValue(of([])),
+    };
+    const mockToastService = {
+      showError: jest.fn(),
+      showInfo: jest.fn(),
+    };
+    const mockAccountService = {
+      userId: 'test-user-id',
+    };
+
     await TestBed.configureTestingModule({
       imports: [ResearchGroupMembersComponent],
+      providers: [
+        { provide: ResearchGroupResourceService, useValue: mockResearchGroupService },
+        { provide: ToastService, useValue: mockToastService },
+        { provide: AccountService, useValue: mockAccountService },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ResearchGroupMembersComponent);
