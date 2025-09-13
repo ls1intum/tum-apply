2245df63bc7fba07446780633e6c194e8f0d9738
Marc Fett <143190400+marc-fett@users.noreply.github.com>
2025-09-13 16:52:10 +0200

    `Development`: Add more registration server APIs (#709)

---
+++ src/main/java/de/tum/cit/aet/usermanagement/constants/OtpPurpose.java
@@ -0,0 +1,11 @@
+package de.tum.cit.aet.usermanagement.constants;
+
+/**
+ * Enumeration of purposes for which a one-time password (OTP) can be used.
+ * - LOGIN: OTP is used for user login authentication.
+ * - REGISTER: OTP is used for new user registration and email verification.
+ */
+public enum OtpPurpose {
+    LOGIN,
+    REGISTER
+}
