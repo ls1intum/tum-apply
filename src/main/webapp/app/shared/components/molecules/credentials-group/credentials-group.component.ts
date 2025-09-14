327e7b350d12a2e91c781e46c11b6c15f50b1d2a
Marc Fett <marc.fett@web.de>
2025-09-14 13:39:27 +0200

    Refactor credentials group component for improved structure and translation support

--- src/main/webapp/app/shared/components/molecules/credentials-group/credentials-group.component.ts
+++ src/main/webapp/app/shared/components/molecules/credentials-group/credentials-group.component.ts
@@ -1,17 +1,17 @@
-import { Component, inject, input, signal } from '@angular/core';
-import { PasswordModule } from 'primeng/password';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
-import { InputTextModule } from 'primeng/inputtext';
-import { MessageModule } from 'primeng/message';
+import { Component, ViewEncapsulation, computed, inject, input, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MessageService } from 'primeng/api';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
+import { DividerModule } from 'primeng/divider';
 
 import { ButtonComponent } from '../../atoms/button/button.component';
 import { StringInputComponent } from '../../atoms/string-input/string-input.component';
 import { PasswordInputComponent } from '../../atoms/password-input/password-input';
 import { environment } from '../../../../environments/environment';
 import { AuthOrchestratorService } from '../../../auth/data-access/auth-orchestrator.service';
+import { AuthIdpButtons } from '../auth-idp-buttons/auth-idp-buttons';
+import { TranslateDirective } from '../../../language';
 
 type SubmitHandler = (email: string, password?: string) => Promise<boolean>;
 
@@ -20,27 +20,29 @@ type SubmitHandler = (email: string, password?: string) => Promise<boolean>;
   standalone: true,
   providers: [MessageService],
   imports: [
-    CommonModule,
-    ReactiveFormsModule,
+    AuthIdpButtons,
     ButtonComponent,
-    PasswordModule,
-    InputTextModule,
-    StringInputComponent,
-    PasswordModule,
-    MessageModule,
-    FormsModule,
+    CommonModule,
+    DividerModule,
     PasswordInputComponent,
+    ReactiveFormsModule,
+    StringInputComponent,
+    TranslateDirective,
     TranslateModule,
   ],
   templateUrl: './credentials-group.component.html',
   styleUrl: './credentials-group.component.scss',
+  encapsulation: ViewEncapsulation.None,
 })
 export class CredentialsGroupComponent {
   authOrchestrator = inject(AuthOrchestratorService);
 
+  isLogin = input<boolean>(true);
   submitHandler = input.required<SubmitHandler>();
   showPassword = input<boolean>(true);
-  submitLabel = input<string>('auth.login.emailLogin.login');
+
+  submitLabel = computed(() => (this.isLogin() ? 'auth.login.buttons.continue' : 'auth.register.buttons.continue'));
+  dividerLabel = computed(() => (this.isLogin() ? 'auth.login.texts.or' : 'auth.register.texts.or'));
 
   isSubmitting = false;
   otpLength = environment.otp.length;
