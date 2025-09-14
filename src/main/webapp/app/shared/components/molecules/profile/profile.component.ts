710fc5d405b2eacf76a9cfe33caa4c7aea7cfb7a
Marc Fett <marc.fett@web.de>
2025-09-14 21:25:40 +0200

    Refactor profile component to improve translation handling and enhance test setup

--- src/main/webapp/app/shared/components/molecules/profile/profile.component.ts
+++ src/main/webapp/app/shared/components/molecules/profile/profile.component.ts
@@ -1,13 +1,15 @@
 import { Component, input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { TranslateModule } from '@ngx-translate/core';
 
 import TranslateDirective from '../../../language/translate.directive';
 import { StringInputComponent } from '../../atoms/string-input/string-input.component';
+import { ButtonComponent } from '../../atoms/button/button.component';
 
 @Component({
   selector: 'jhi-profile',
-  imports: [CommonModule, ReactiveFormsModule, TranslateDirective, StringInputComponent],
+  imports: [CommonModule, ButtonComponent, ReactiveFormsModule, TranslateDirective, TranslateModule, StringInputComponent],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss',
 })
