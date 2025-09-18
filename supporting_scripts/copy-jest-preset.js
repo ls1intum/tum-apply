61ba5c35fa5862d741f562fa940278708ad09e2c
Sarah Douglas <sarah.douglas@gmx.de>
2025-09-18 17:43:05 +0200

    fix: client-tests

--- supporting_scripts/copy-jest-preset.js
+++ supporting_scripts/copy-jest-preset.js
@@ -1,12 +1,19 @@
-const fs = require('fs-extra');
 const path = require('path');
+const fs = require('fs-extra');
 
 const src = path.join(__dirname, '..', 'node_modules', 'jest-preset-angular');
-const dest = path.join(__dirname, '..', 'node_modules', '@angular-builders', 'jest', 'node_modules', 'jest-preset-angular');
+const dest = path.join(
+  __dirname,
+  '..',
+  'node_modules',
+  '@angular-builders',
+  'jest',
+  'node_modules',
+  'jest-preset-angular'
+);
 
 fs.copy(src, dest, { overwrite: true })
-    .then(() => console.log('jest-preset-angular copied successfully'))
-    .catch(err => {
-        console.error('Error copying jest-preset-angular:', err);
-        process.exit(1);
-    });
+  .catch((err) => {
+    console.error('Error copying jest-preset-angular:', err);
+    process.exit(1);
+  });
