2ad9fbd4196e08e8d3aa8568afd8037b8513d422
Abi107717 <a.abinaya.sivaguru@gmail.com>
2025-09-18 11:41:44 +0200

    add other fields (optional vlaues) in ResearchGroupCreationDTO

--- src/main/java/de/tum/cit/aet/usermanagement/dto/ResearchGroupCreationDTO.java
+++ src/main/java/de/tum/cit/aet/usermanagement/dto/ResearchGroupCreationDTO.java
@@ -1,6 +1,7 @@
 package de.tum.cit.aet.usermanagement.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
@@ -8,5 +9,14 @@ import jakarta.validation.constraints.Size;
 public record ResearchGroupCreationDTO(
     @NotBlank @Size(min = 3, max = 150) String name,
     @NotBlank @Size(min = 3, max = 150) String headName,
-    @NotBlank @Size(min = 3, max = 150) String universityId
+    @NotBlank @Size(min = 3, max = 7) String universityId,
+    @Email String email,
+    String abbreviation,
+    String website,
+    String school,
+    String description,
+    String defaultFieldOfStudies,
+    String street,
+    String postalCode,
+    String city
 ) {}
