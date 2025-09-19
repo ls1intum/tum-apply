5bf576a32861eadff0f3631ca57843ecd3a4e69e
Kiara Copony <ge65min@mytum.de>
2025-09-19 10:34:12 +0200

    fix client tests

--- src/main/webapp/app/shared/components/atoms/sorting/sorting.spec.ts
+++ src/main/webapp/app/shared/components/atoms/sorting/sorting.spec.ts
@@ -1,4 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  MissingTranslationHandler,
+  TranslateCompiler,
+  TranslateLoader,
+  TranslateModule,
+  TranslateParser,
+  TranslateStore,
+} from '@ngx-translate/core';
+import { missingTranslationHandler } from 'app/config/translation.config';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faArrowDown19, faArrowDownAZ, faArrowUp19, faArrowUpAZ, faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
 import { Sorting } from './sorting';
 
@@ -8,11 +19,30 @@ describe('Sorting', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Sorting],
+      imports: [
+        Sorting,
+        TranslateModule.forRoot({
+          missingTranslationHandler: {
+            provide: MissingTranslationHandler,
+            useFactory: missingTranslationHandler,
+          },
+        }),
+      ],
+      providers: [TranslateStore, TranslateLoader, TranslateCompiler, TranslateParser],
     }).compileComponents();
 
     fixture = TestBed.createComponent(Sorting);
     component = fixture.componentInstance;
+
+    fixture.componentRef.setInput('sortableFields', [{ displayName: 'Name', fieldName: 'name', type: 'TEXT' }]);
+
+    const library = TestBed.inject(FaIconLibrary);
+    library.addIcons(faChevronDown);
+    library.addIcons(faArrowUpAZ);
+    library.addIcons(faArrowUp19);
+    library.addIcons(faArrowDownAZ);
+    library.addIcons(faArrowDown19);
+
     fixture.detectChanges();
   });
 
