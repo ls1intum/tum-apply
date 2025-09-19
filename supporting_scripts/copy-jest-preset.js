e0d7ad53b099ce829b066a7061d57869dfee6ab2
Moritz <moritzschmidt1@gmail.com>
2025-09-19 14:36:03 +0200

    fix prettier

--- supporting_scripts/copy-jest-preset.js
+++ supporting_scripts/copy-jest-preset.js
@@ -5,8 +5,7 @@ const fs = require('fs-extra');
 const src = path.join(__dirname, '..', 'node_modules', 'jest-preset-angular');
 const dest = path.join(__dirname, '..', 'node_modules', '@angular-builders', 'jest', 'node_modules', 'jest-preset-angular');
 
-fs.copy(src, dest, { overwrite: true })
-  .catch((err) => {
-    console.error('Error copying jest-preset-angular:', err);
-    process.exit(1);
-  });
+fs.copy(src, dest, { overwrite: true }).catch(err => {
+  console.error('Error copying jest-preset-angular:', err);
+  process.exit(1);
+});
