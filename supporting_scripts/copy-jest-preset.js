990673b5d980a1c082cb2110c28d3ebbeabbd7ad
Moritz <moritzschmidt1@gmail.com>
2025-09-19 14:54:06 +0200

    make copy-jest-preset.js fail save

--- supporting_scripts/copy-jest-preset.js
+++ supporting_scripts/copy-jest-preset.js
@@ -5,7 +5,16 @@ const fs = require('fs-extra');
 const src = path.join(__dirname, '..', 'node_modules', 'jest-preset-angular');
 const dest = path.join(__dirname, '..', 'node_modules', '@angular-builders', 'jest', 'node_modules', 'jest-preset-angular');
 
-fs.copy(src, dest, { overwrite: true }).catch(err => {
-  console.error('Error copying jest-preset-angular:', err);
-  process.exit(1);
-});
+(async () => {
+  try {
+    if (await fs.pathExists(src)) {
+      await fs.ensureDir(path.dirname(dest));
+      await fs.copy(src, dest, { overwrite: true });
+      console.log('jest-preset-angular copied successfully.');
+    } else {
+      console.warn('jest-preset-angular not found, skipping copy.');
+    }
+  } catch (err) {
+    console.warn('Warning: could not copy jest-preset-angular:', err.message);
+  }
+})();
