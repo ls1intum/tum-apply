afc364a54795ccc5f2cac40bf1536fa6e416fadf
Abi107717 <a.abinaya.sivaguru@gmail.com>
2025-09-21 11:24:25 +0200

    add nullable=false in ResearchGroup.java

--- src/main/java/de/tum/cit/aet/usermanagement/domain/ResearchGroup.java
+++ src/main/java/de/tum/cit/aet/usermanagement/domain/ResearchGroup.java
@@ -58,8 +58,9 @@ public class ResearchGroup extends AbstractAuditingEntity {
     @Column(name = "city")
     private String city;
 
-    @Column(name = "university_id")
+    @Column(name = "university_id", nullable = false)
     private String universityId;
+
 
     @OneToMany(mappedBy = "researchGroup", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<UserResearchGroupRole> userRoles = new HashSet<>();
