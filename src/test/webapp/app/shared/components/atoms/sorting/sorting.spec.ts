705a1d5e0d72904f5b38ec8e26d85f6455b7804e
Kiara Copony <ge65min@mytum.de>
2025-09-23 10:17:57 +0200

    use provideFontAwesomeTesting for icons

--- src/test/webapp/app/shared/components/atoms/sorting/sorting.spec.ts
+++ src/test/webapp/app/shared/components/atoms/sorting/sorting.spec.ts
@@ -8,9 +8,8 @@ import {
   TranslateStore,
 } from '@ngx-translate/core';
 import { missingTranslationHandler } from 'app/config/translation.config';
-import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
-import { faArrowDown19, faArrowDownAZ, faArrowUp19, faArrowUpAZ, faChevronDown } from '@fortawesome/free-solid-svg-icons';
 import { Sorting } from 'app/shared/components/atoms/sorting/sorting';
+import { provideFontAwesomeTesting } from 'util/fontawesome.testing';
 
 describe('Sorting', () => {
   let component: Sorting;
@@ -27,20 +26,13 @@ describe('Sorting', () => {
           },
         }),
       ],
-      providers: [TranslateStore, TranslateLoader, TranslateCompiler, TranslateParser],
+      providers: [TranslateStore, TranslateLoader, TranslateCompiler, TranslateParser, provideFontAwesomeTesting()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(Sorting);
     component = fixture.componentInstance;
 
     fixture.componentRef.setInput('sortableFields', [{ displayName: 'Name', fieldName: 'name', type: 'TEXT' }]);
-
-    const library = TestBed.inject(FaIconLibrary);
-    library.addIcons(faChevronDown);
-    library.addIcons(faArrowUpAZ);
-    library.addIcons(faArrowUp19);
-    library.addIcons(faArrowDownAZ);
-    library.addIcons(faArrowDown19);
 
     fixture.detectChanges();
   });
