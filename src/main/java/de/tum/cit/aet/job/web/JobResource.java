c30c456b2cd931b2f1e1ca3bdf783ef18f202c12
Marc Fett <marc.fett@web.de>
2025-09-26 13:57:08 +0200

    remove logger

--- src/main/java/de/tum/cit/aet/job/web/JobResource.java
+++ src/main/java/de/tum/cit/aet/job/web/JobResource.java
@@ -6,13 +6,12 @@ import de.tum.cit.aet.job.constants.JobState;
 import de.tum.cit.aet.job.dto.*;
 import de.tum.cit.aet.job.service.JobService;
 import jakarta.validation.Valid;
-import java.util.UUID;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 /**
  * REST controller for managing job postings.
@@ -22,7 +21,6 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/jobs")
 public class JobResource {
 
-    private static final Logger log = LoggerFactory.getLogger(JobResource.class);
     private final JobService jobService;
 
     public JobResource(JobService jobService) {
