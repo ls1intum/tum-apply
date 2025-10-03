ea022e1bdd2b3dac0ff5e358dd054c9d28115d66
aniruddhzaveri <aniruddh.zaveri@tum.de>
2025-10-03 14:15:24 +0200

    fix coverage settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 
 import angular from '@analogjs/vite-plugin-angular';
 import path from 'node:path';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig(({mode}) => ({
+export default defineConfig(({ mode }) => ({
   plugins: [
     angular(),
     tsconfigPaths({
@@ -23,6 +23,7 @@ export default defineConfig(({mode}) => ({
       reportsDirectory: 'build/test-results/lcov-report',
       provider: 'v8',
       all: true,
+      enabled: true,
       exclude: ['**/node_modules/**', '**/generated/**', '**/*.spec.ts', '**/*.test.ts'],
       check: {
         global: {
