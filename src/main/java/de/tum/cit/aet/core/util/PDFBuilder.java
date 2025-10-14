bf209a0955a5ee2aa1bfc2e6f1ec3b4bc92d771e
Kiara Copony <ge65min@mytum.de>
2025-10-14 15:41:08 +0200

    add header to PDF file

--- src/main/java/de/tum/cit/aet/core/util/PDFBuilder.java
+++ src/main/java/de/tum/cit/aet/core/util/PDFBuilder.java
@@ -3,23 +3,37 @@ package de.tum.cit.aet.core.util;
 import com.itextpdf.html2pdf.ConverterProperties;
 import com.itextpdf.html2pdf.HtmlConverter;
 import com.itextpdf.io.font.constants.StandardFonts;
+import com.itextpdf.io.image.ImageDataFactory;
+import com.itextpdf.kernel.colors.Color;
+import com.itextpdf.kernel.colors.DeviceRgb;
 import com.itextpdf.kernel.font.PdfFont;
 import com.itextpdf.kernel.font.PdfFontFactory;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.Document;
+import com.itextpdf.layout.borders.Border;
+import com.itextpdf.layout.element.Cell;
 import com.itextpdf.layout.element.IBlockElement;
 import com.itextpdf.layout.element.IElement;
+import com.itextpdf.layout.element.Image;
 import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.element.Table;
 import com.itextpdf.layout.element.Text;
+import com.itextpdf.layout.properties.HorizontalAlignment;
+import com.itextpdf.layout.properties.VerticalAlignment;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 
 public class PDFBuilder {
+
+    Color primaryColor = new DeviceRgb(0x18, 0x72, 0xDD);
+
+    private static final String LOGO_PATH = "images/tum-logo-blue.png";
 
     private final String heading;
     private final List<Section> sections;
@@ -56,6 +70,8 @@ public class PDFBuilder {
             // Load bold font
             PdfFont boldFont = PdfFontFactory.createFont(StandardFonts.HELVETICA_BOLD);
             PdfFont normalFont = PdfFontFactory.createFont(StandardFonts.HELVETICA);
+
+            addLogoHeader(document, boldFont);
 
             // Main heading
             Paragraph mainHeadingParagraph = new Paragraph(heading).setFont(boldFont).setFontSize(20).setMarginBottom(16);
@@ -95,5 +111,46 @@ public class PDFBuilder {
         }
 
         return new ByteArrayResource(outputStream.toByteArray());
+    }
+
+    private void addLogoHeader(Document document, PdfFont boldFont) throws IOException {
+        float logoSize = 35;
+
+        Table headerTable = new Table(new float[] { 1, 4 });
+        headerTable.setBorder(Border.NO_BORDER);
+        headerTable.setHorizontalAlignment(HorizontalAlignment.LEFT);
+        try {
+            URL logoUrl = getClass().getClassLoader().getResource(LOGO_PATH);
+
+            if (logoUrl != null) {
+                Image logo = new Image(ImageDataFactory.create(logoUrl));
+                logo.scaleToFit(logoSize, logoSize);
+
+                Cell logoCell = new Cell()
+                    .add(logo)
+                    .setBorder(Border.NO_BORDER)
+                    .setVerticalAlignment(VerticalAlignment.MIDDLE)
+                    .setPaddingRight(5);
+
+                headerTable.addCell(logoCell);
+            } else {
+                System.err.println("Logo not found at path: " + LOGO_PATH);
+                headerTable.addCell(new Cell().setBorder(Border.NO_BORDER));
+            }
+        } catch (Exception e) {
+            System.err.println("Could not load logo image: " + e.getMessage());
+        }
+
+        Paragraph appNamePara = new Paragraph(new Text("Apply").setFont(boldFont).setFontSize(12)).setFontColor(primaryColor).setMargin(0);
+
+        Cell textCell = new Cell()
+            .add(appNamePara)
+            .setBorder(Border.NO_BORDER)
+            .setVerticalAlignment(VerticalAlignment.MIDDLE)
+            .setPaddingLeft(0);
+
+        headerTable.addCell(textCell);
+
+        document.add(headerTable);
     }
 }
