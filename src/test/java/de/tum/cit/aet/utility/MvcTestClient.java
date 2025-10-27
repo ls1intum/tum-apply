93dac6194a6ecc3abbe51f0bddd8c995d2c922f3
Sarah Douglas <sarah.douglas@gmx.de>
2025-10-27 03:02:29 +0100

    fix: tests

--- src/test/java/de/tum/cit/aet/utility/MvcTestClient.java
+++ src/test/java/de/tum/cit/aet/utility/MvcTestClient.java
@@ -116,6 +116,8 @@ public class MvcTestClient {
             for (MockMultipartFile file : files) {
                 builder.file(file);
             }
+            // Apply default Accept header and RequestPostProcessors (e.g., JWT)
+            applyDefaults(builder);
             MvcResult result = mockMvc.perform(builder).andExpect(status().is(expectedStatus)).andReturn();
 
             String body = result.getResponse().getContentAsString();
