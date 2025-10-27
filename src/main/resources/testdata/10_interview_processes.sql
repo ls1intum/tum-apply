f400e4f64a6bca11006166ef95d1b9387bfd4d3f
Abi107717 <a.abinaya.sivaguru@gmail.com>
2025-10-27 16:04:20 +0100

    fix tesdata

--- src/main/resources/testdata/10_interview_processes.sql
+++ src/main/resources/testdata/10_interview_processes.sql
@@ -8,18 +8,26 @@
 
 -- Clean up existing entries (for consistent seeding)
 DELETE
-FROM dbo.interview_processes
+FROM interview_processes
 WHERE id LIKE '00000000-0000-0000-0000-000000030%';
 
--- Insert interview_processes only for professor1@tumapply.local's jobs
--- Research Group 1 jobs (professor_id: 00000000-0000-0000-0000-000000000102)
-INSERT INTO dbo.interview_processes (id, job_id, created_at, last_modified_at)
+-- Insert example interview processes linked to existing jobs
+INSERT INTO interview_processes (id, job_id, created_at, last_modified_at)
 VALUES
   -- Job 20001: Gamification in Education Intern (PUBLISHED)
-  ('00000000-0000-0000-0000-000000030001', '00000000-0000-0000-0000-000000020001', '2025-01-15 09:00:00', '2025-01-15 09:00:00'),
+  ('00000000-0000-0000-0000-000000030001',
+   '00000000-0000-0000-0000-000000020001',
+   '2025-01-15 09:00:00',
+   '2025-01-15 09:00:00'),
 
   -- Job 20002: Learning Analytics Researcher (DRAFT)
-  ('00000000-0000-0000-0000-000000030002', '00000000-0000-0000-0000-000000020002', '2025-02-10 11:00:00', '2025-02-10 11:00:00'),
+  ('00000000-0000-0000-0000-000000030002',
+   '00000000-0000-0000-0000-000000020002',
+   '2025-02-10 11:00:00',
+   '2025-02-10 11:00:00'),
 
   -- Job 20005: Automated Grading Researcher (APPLICANT_FOUND)
-  ('00000000-0000-0000-0000-000000030003', '00000000-0000-0000-0000-000000020005', '2025-01-15 09:00:00', '2025-01-15 09:00:00');
+  ('00000000-0000-0000-0000-000000030003',
+   '00000000-0000-0000-0000-000000020005',
+   '2025-01-15 09:00:00',
+   '2025-01-15 09:00:00');
