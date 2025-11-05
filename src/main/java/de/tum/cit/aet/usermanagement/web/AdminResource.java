1391058874840029d7c986c39788760a1a4ce1f7
Marc Fett <marc.fett@web.de>
2025-11-05 21:41:45 +0100

    replace with @admin

--- src/main/java/de/tum/cit/aet/usermanagement/web/AdminResource.java
+++ src/main/java/de/tum/cit/aet/usermanagement/web/AdminResource.java
@@ -7,7 +7,6 @@ import de.tum.cit.aet.usermanagement.service.ResearchGroupService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/api/admin")
-@PreAuthorize("hasRole('ADMIN')")
+@Admin
 @RequiredArgsConstructor
 public class AdminResource {
 
