01aa1915e18456f6d16768b7d39c8a7cd8f94e3b
Bofan Zhu <56017927+Bofan-Zhu@users.noreply.github.com>
2025-11-07 17:29:30 +0100

    `Usability`: Improve landing pages responsiveness (#1156)

--- src/main/webapp/app/shared/pages/landing-page/application-steps-section/application-step/application-step/application-step.component.ts
+++ src/main/webapp/app/shared/pages/landing-page/application-steps-section/application-step/application-step/application-step.component.ts
@@ -1,15 +1,13 @@
 import { Component, input } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-
-import TranslateDirective from '../../../../../../shared/language/translate.directive';
-import { ButtonComponent } from '../../../../../components/atoms/button/button.component';
+import TranslateDirective from 'app/shared/language/translate.directive';
+import { ButtonComponent } from 'app/shared/components/atoms/button/button.component';
 
 @Component({
   selector: 'jhi-application-step',
   imports: [FontAwesomeModule, TranslateModule, ButtonComponent, TranslateDirective],
   templateUrl: './application-step.component.html',
-  styleUrl: './application-step.component.scss',
 })
 export class ApplicationStepComponent {
   icon = input<string>('search');
