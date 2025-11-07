a42e4c05cac57fd969af1f54193f90750a79bcd2
Kiara Copony <ge65min@mytum.de>
2025-11-07 12:55:14 +0100

    comment out converted grade for professor view

--- src/main/webapp/app/evaluation/application-detail/application-detail.component.ts
+++ src/main/webapp/app/evaluation/application-detail/application-detail.component.ts
@@ -176,7 +176,7 @@ export class ApplicationDetailComponent {
       { labelKey: 'evaluation.details.educationGrade', value: applicant.bachelorGrade },
     ];
 
-    const converted = this.getGradeItem(
+    /* const converted = this.getGradeItem(
       applicant.bachelorGrade,
       applicant.bachelorGradeUpperLimit,
       applicant.bachelorGradeLowerLimit,
@@ -184,7 +184,7 @@ export class ApplicationDetailComponent {
       'evaluation.details.converterTooltip',
     );
 
-    items.push(...converted);
+    items.push(...converted);*/
     return items;
   }
 
@@ -196,7 +196,7 @@ export class ApplicationDetailComponent {
       { labelKey: 'evaluation.details.educationGrade', value: applicant.masterGrade },
     ];
 
-    const converted = this.getGradeItem(
+    /* const converted = this.getGradeItem(
       applicant.masterGrade,
       applicant.masterGradeUpperLimit,
       applicant.masterGradeLowerLimit,
@@ -204,7 +204,7 @@ export class ApplicationDetailComponent {
       'evaluation.details.converterTooltip',
     );
 
-    items.push(...converted);
+    items.push(...converted);*/
     return items;
   }
 
