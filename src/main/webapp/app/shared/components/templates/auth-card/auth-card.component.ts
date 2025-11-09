376615cec6d9de8ad3ec41383c14ca139e584ffb
Melissa <ge94zec@tum.de>
2025-11-09 22:35:37 +0100

    Added braces to arrow function

--- src/main/webapp/app/shared/components/templates/auth-card/auth-card.component.ts
+++ src/main/webapp/app/shared/components/templates/auth-card/auth-card.component.ts
@@ -30,7 +30,7 @@ export class AuthCardComponent {
         severity: 'secondary',
         label: undefined,
         disabled: false,
-        onClick: () => this.onClose(),
+        onClick: () => { this.onClose(); },
       },
     ],
   };
