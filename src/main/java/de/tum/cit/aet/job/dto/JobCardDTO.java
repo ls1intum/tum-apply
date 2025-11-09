f570f43b9a719c27f45307885c3ec4d742a02fe9
Bofan Zhu <bofan.zhu@tum.de>
2025-11-09 18:44:13 +0100

    add additional card info

--- src/main/java/de/tum/cit/aet/job/dto/JobCardDTO.java
+++ src/main/java/de/tum/cit/aet/job/dto/JobCardDTO.java
@@ -20,7 +20,9 @@ public record JobCardDTO(
     Integer workload,
     LocalDate startDate,
     String relativeTimeEnglish,
-    String relativeTimeGerman
+    String relativeTimeGerman,
+    String fundingType,
+    Integer contractDuration
 ) {
     public JobCardDTO(
         @NotNull UUID jobId,
@@ -32,7 +34,9 @@ public record JobCardDTO(
         ApplicationState applicationState,
         Integer workload,
         LocalDate startDate,
-        LocalDate endDate
+        LocalDate endDate,
+        String fundingType,
+        Integer contractDuration
     ) {
         this(
             jobId,
@@ -45,7 +49,9 @@ public record JobCardDTO(
             workload,
             startDate,
             UiTextFormatter.getTimeLeftLabelEnglish(endDate),
-            UiTextFormatter.getTimeLeftLabelGerman(endDate)
+            UiTextFormatter.getTimeLeftLabelGerman(endDate),
+            fundingType,
+            contractDuration
         );
     }
 }
