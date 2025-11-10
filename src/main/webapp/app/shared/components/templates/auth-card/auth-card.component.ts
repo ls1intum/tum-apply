c401df78efbaf5bbdd6626b515efc8e8ab1d79b0
Melissa <ge94zec@tum.de>
2025-11-10 15:55:39 +0100

    Changed html button-group to button-component and fixed ikon color noticeability

--- src/main/webapp/app/shared/components/templates/auth-card/auth-card.component.ts
+++ src/main/webapp/app/shared/components/templates/auth-card/auth-card.component.ts
@@ -5,35 +5,17 @@ import { DividerModule } from 'primeng/divider';
 import { Login } from '../../organisms/login/login';
 import { Registration } from '../../organisms/registration/registration';
 import { AuthOrchestratorService } from '../../../../core/auth/auth-orchestrator.service';
-import {ButtonGroupData} from 'app/shared/components/molecules/button-group/button-group.component';
 import { ButtonComponent } from 'app/shared/components/atoms/button/button.component';
-import ButtonGroupComponent from 'app/shared/components/molecules/button-group/button-group.component';
-
 
 @Component({
   selector: 'jhi-auth-card',
   standalone: true,
-  imports: [CommonModule, DividerModule, Login, Registration, ButtonGroupComponent, ButtonComponent],
+  imports: [CommonModule, DividerModule, Login, Registration, ButtonComponent],
   templateUrl: './auth-card.component.html',
   styleUrls: ['./auth-card.component.scss'],
 })
 export class AuthCardComponent {
   readonly authOrchestrator = inject(AuthOrchestratorService);
-
-  buttonGroupData: ButtonGroupData = {
-    direction: 'horizontal',
-    fullWidth: false,
-    buttons: [
-      {
-        icon: 'xmark',
-        variant: 'text',
-        severity: 'secondary',
-        label: undefined,
-        disabled: false,
-        onClick: () => { this.onClose(); },
-      },
-    ],
-  };
 
   onClose(): void {
     this.authOrchestrator.close?.();
