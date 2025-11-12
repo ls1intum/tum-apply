8ebe5f756fa689631a3b318f101dbee8745aaf53
Sarah Douglas <sarah.douglas@gmx.de>
2025-11-12 17:38:45 +0100

    `Development`: Change list to page for application overview (#1213)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Aniruddh Zaveri <92953467+az108@users.noreply.github.com>

--- src/main/java/de/tum/cit/aet/application/repository/ApplicationEntityRepository.java
+++ src/main/java/de/tum/cit/aet/application/repository/ApplicationEntityRepository.java
@@ -1,9 +1,10 @@
 package de.tum.cit.aet.application.repository;
 
 import de.tum.cit.aet.application.domain.dto.ApplicationOverviewDTO;
-import java.util.List;
 import java.util.UUID;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface ApplicationEntityRepository {
-    public List<ApplicationOverviewDTO> findApplicationsByApplicant(UUID applicantId, int pageNumber, int pageSize);
+    Page<ApplicationOverviewDTO> findApplicationsByApplicant(UUID applicantId, Pageable pageable);
 }
