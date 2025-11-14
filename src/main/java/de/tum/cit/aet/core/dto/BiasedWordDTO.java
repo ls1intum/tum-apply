18c2c0763a8e0a19b06157015da60384e6691e55
Kiara Copony <ge65min@mytum.de>
2025-11-14 12:03:17 +0100

    Change BiasedWordDTO and GenderBiasResponse to Java records

--- src/main/java/de/tum/cit/aet/core/dto/BiasedWordDTO.java
+++ src/main/java/de/tum/cit/aet/core/dto/BiasedWordDTO.java
@@ -3,33 +3,4 @@ package de.tum.cit.aet.core.dto;
 /**
  * DTO for individual biased word
  */
-public class BiasedWordDTO {
-
-    private String word;
-    private String type; // "masculine" or "feminine"
-
-    // Constructors
-    public BiasedWordDTO() {}
-
-    public BiasedWordDTO(String word, String type) {
-        this.word = word;
-        this.type = type;
-    }
-
-    // Getters and Setters
-    public String getWord() {
-        return word;
-    }
-
-    public void setWord(String word) {
-        this.word = word;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-}
+public record BiasedWordDTO(String word, String type) {}
