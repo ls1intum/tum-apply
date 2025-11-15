2c1ee5f586d594b4638797df23a2ce31f838c1f3
Kiara Copony <ge65min@mytum.de>
2025-11-15 11:25:58 +0100

    make request to java record

--- src/main/java/de/tum/cit/aet/core/dto/GenderBiasAnalysisRequest.java
+++ src/main/java/de/tum/cit/aet/core/dto/GenderBiasAnalysisRequest.java
@@ -6,33 +6,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
  * Request DTO for gender bias analysis
  */
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class GenderBiasAnalysisRequest {
-
-    private String text;
-    private String language; // "en" or "de"
-
-    // Constructors
-    public GenderBiasAnalysisRequest() {}
-
-    public GenderBiasAnalysisRequest(String text, String language) {
-        this.text = text;
-        this.language = language;
-    }
-
-    // Getters and Setters
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public String getLanguage() {
-        return language;
-    }
-
-    public void setLanguage(String language) {
-        this.language = language;
-    }
-}
+public record GenderBiasAnalysisRequest(
+    String text,
+    String language // "en" or "de"
+) {}
