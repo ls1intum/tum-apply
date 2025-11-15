6c47f7f5450d3da7c1b0600e331da4df433f4a73
Bofan Zhu <bofan.zhu@tum.de>
2025-11-15 20:42:29 +0100

    update

--- src/main/java/de/tum/cit/aet/job/service/JobImageHelper.java
+++ src/main/java/de/tum/cit/aet/job/service/JobImageHelper.java
@@ -8,41 +8,6 @@ import org.springframework.stereotype.Component;
 
 /**
  * Helper class for managing job images in JobService
- * 
- * Usage example in JobService:
- * 
- * When creating a job:
- * <pre>
- * public Job createJob(JobFormDTO dto) {
- *     Job job = new Job();
- *     // ... set other fields ...
- *     
- *     // Set image if provided
- *     if (dto.imageId() != null) {
- *         job.setImage(jobImageHelper.getImageForJob(dto.imageId()));
- *     }
- *     
- *     return jobRepository.save(job);
- * }
- * </pre>
- * 
- * When updating a job:
- * <pre>
- * public Job updateJob(UUID jobId, JobFormDTO dto) {
- *     Job job = jobRepository.findById(jobId).orElseThrow();
- *     // ... update other fields ...
- *     
- *     // Handle image update/replacement
- *     Image oldImage = job.getImage();
- *     Image newImage = dto.imageId() != null 
- *         ? jobImageHelper.getImageForJob(dto.imageId()) 
- *         : null;
- *     
- *     job.setImage(jobImageHelper.replaceJobImage(oldImage, newImage));
- *     
- *     return jobRepository.save(job);
- * }
- * </pre>
  */
 @Component
 public class JobImageHelper {
