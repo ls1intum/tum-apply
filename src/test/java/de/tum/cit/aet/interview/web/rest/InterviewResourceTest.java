87f194aaf0a4648f30687ae02c04d5b146343246
Abi107717 <a.abinaya.sivaguru@gmail.com>
2025-11-19 11:24:11 +0100

    add server test

---
+++ src/test/java/de/tum/cit/aet/interview/web/rest/InterviewResourceTest.java
@@ -0,0 +1,144 @@
+package de.tum.cit.aet.interview.web.rest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import de.tum.cit.aet.AbstractResourceTest;
+import de.tum.cit.aet.interview.domain.InterviewProcess;
+import de.tum.cit.aet.interview.dto.InterviewOverviewDTO;
+import de.tum.cit.aet.interview.repository.InterviewProcessRepository;
+import de.tum.cit.aet.job.constants.JobState;
+import de.tum.cit.aet.job.domain.Job;
+import de.tum.cit.aet.job.repository.JobRepository;
+import de.tum.cit.aet.usermanagement.domain.ResearchGroup;
+import de.tum.cit.aet.usermanagement.domain.User;
+import de.tum.cit.aet.usermanagement.repository.ResearchGroupRepository;
+import de.tum.cit.aet.usermanagement.repository.UserRepository;
+import de.tum.cit.aet.utility.DatabaseCleaner;
+import de.tum.cit.aet.utility.MvcTestClient;
+import de.tum.cit.aet.utility.security.JwtPostProcessors;
+import de.tum.cit.aet.utility.testdata.JobTestData;
+import de.tum.cit.aet.utility.testdata.ResearchGroupTestData;
+import de.tum.cit.aet.utility.testdata.UserTestData;
+import java.time.LocalDate;
+import java.util.UUID;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.test.context.support.WithMockUser;
+
+class InterviewResourceTest extends AbstractResourceTest {
+
+    @Autowired
+    private InterviewProcessRepository interviewProcessRepository;
+
+    @Autowired
+    private JobRepository jobRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ResearchGroupRepository researchGroupRepository;
+
+    @Autowired
+    private DatabaseCleaner databaseCleaner;
+
+    @Autowired
+    private MvcTestClient api;
+
+    private User professor;
+    private Job job;
+    private InterviewProcess interviewProcess;
+    private ResearchGroup researchGroup;
+
+    @BeforeEach
+    void setup() {
+        databaseCleaner.clean();
+
+        researchGroup = ResearchGroupTestData.savedAll(
+            researchGroupRepository,
+            "Algorithms Group",
+            "Prof. Doe",
+            "alg@example.com",
+            "ALG",
+            "CS",
+            "We do cool stuff",
+            "alg@example.com",
+            "80333",
+            "CIT",
+            "Arcisstr. 21",
+            "https://alg.tum.de",
+            "ACTIVE"
+        );
+
+        professor = UserTestData.savedProfessorAll(
+            userRepository,
+            researchGroup,
+            null,
+            "prof.doe@tum.de",
+            "John",
+            "Doe",
+            "en",
+            "+49 89 1234",
+            "https://doe.tum.de",
+            "https://linkedin.com/in/doe",
+            "DE",
+            null,
+            "männlich",
+            UUID.randomUUID().toString().replace("-", "").substring(0, 7)
+        );
+
+        job = JobTestData.saved(jobRepository, professor, researchGroup, "Software Engineer", JobState.PUBLISHED, LocalDate.now());
+
+        interviewProcess = new InterviewProcess();
+        interviewProcess.setJob(job);
+        interviewProcess = interviewProcessRepository.save(interviewProcess);
+    }
+
+    @Test
+    @WithMockUser(roles = "PROFESSOR")
+    void getInterviewProcessDetails_returnsCorrectDetails() {
+        InterviewOverviewDTO details = api
+            .with(JwtPostProcessors.jwtUser(professor.getUserId(), "ROLE_PROFESSOR"))
+            .getAndRead("/api/interviews/processes/" + interviewProcess.getId(), null, InterviewOverviewDTO.class, 200);
+
+        assertThat(details.jobId()).isEqualTo(job.getJobId());
+        assertThat(details.jobTitle()).isEqualTo(job.getTitle());
+        // Stats will be 0 since we didn't create applications
+        assertThat(details.totalInterviews()).isZero();
+    }
+
+    @Test
+    @WithMockUser(roles = "PROFESSOR")
+    void getInterviewProcessDetails_forbiddenForOtherUser() {
+        // Create another professor
+        User otherProfessor = UserTestData.savedProfessorAll(
+            userRepository,
+            researchGroup,
+            null,
+            "other.prof@tum.de",
+            "Jane",
+            "Doe",
+            "en",
+            "+49 89 5678",
+            "https://jane.tum.de",
+            "https://linkedin.com/in/jane",
+            "DE",
+            null,
+            "weiblich",
+            UUID.randomUUID().toString().replace("-", "").substring(0, 7)
+        );
+
+        api
+            .with(JwtPostProcessors.jwtUser(otherProfessor.getUserId(), "ROLE_PROFESSOR"))
+            .getAndRead("/api/interviews/processes/" + interviewProcess.getId(), null, InterviewOverviewDTO.class, 403);
+    }
+
+    @Test
+    @WithMockUser(roles = "PROFESSOR")
+    void getInterviewProcessDetails_notFoundForNonExistentId() {
+        api
+            .with(JwtPostProcessors.jwtUser(professor.getUserId(), "ROLE_PROFESSOR"))
+            .getAndRead("/api/interviews/processes/" + UUID.randomUUID(), null, InterviewOverviewDTO.class, 404);
+    }
+}
