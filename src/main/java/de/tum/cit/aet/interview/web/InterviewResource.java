5c968d0390de7e546c7b3d36c4dfcbb3a90b1b71
Abi107717 <a.abinaya.sivaguru@gmail.com>
2025-11-21 17:45:33 +0100

    add logging

--- src/main/java/de/tum/cit/aet/interview/web/InterviewResource.java
+++ src/main/java/de/tum/cit/aet/interview/web/InterviewResource.java
@@ -1,11 +1,11 @@
 package de.tum.cit.aet.interview.web;
 
 import de.tum.cit.aet.core.security.annotations.Professor;
-import de.tum.cit.aet.core.security.annotations.ProfessorOrAdmin;
 import de.tum.cit.aet.interview.dto.InterviewOverviewDTO;
 import de.tum.cit.aet.interview.service.InterviewService;
 import java.util.List;
 import java.util.UUID;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
  * REST controller for managing interview processes.
  * Provides endpoints for interview overview and management.
  */
+@Slf4j
 @RestController
 @RequestMapping("/api/interviews")
 public class InterviewResource {
@@ -38,7 +39,9 @@ public class InterviewResource {
     @Professor
     @GetMapping("/overview")
     public ResponseEntity<List<InterviewOverviewDTO>> getInterviewOverview() {
+        log.info("REST request to get all interview processes{}");
         List<InterviewOverviewDTO> overview = interviewService.getInterviewOverview();
+        log.info("Returning all interview processes");
         return ResponseEntity.ok(overview);
     }
 
@@ -51,7 +54,9 @@ public class InterviewResource {
     @Professor
     @GetMapping("/processes/{processId}")
     public ResponseEntity<InterviewOverviewDTO> getInterviewProcessDetails(@PathVariable UUID processId) {
+        log.info("REST request to get interview process id {}", processId);
         InterviewOverviewDTO details = interviewService.getInterviewProcessDetails(processId);
+        log.info("Returning {} interview processes", processId);
         return ResponseEntity.ok(details);
     }
 }
