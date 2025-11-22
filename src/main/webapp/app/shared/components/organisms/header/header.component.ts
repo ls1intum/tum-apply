003fc3f8b3bde897af363ade2aae5efb5fc6de54
Bofan Zhu <bofan.zhu@tum.de>
2025-11-22 23:48:47 +0100

    adjust colors

--- src/main/webapp/app/shared/components/organisms/header/header.component.ts
+++ src/main/webapp/app/shared/components/organisms/header/header.component.ts
@@ -38,11 +38,11 @@ import { IdpProvider } from '../../../../core/auth/keycloak-authentication.servi
 export class HeaderComponent {
   bodyClassChanges$ = fromEventPattern<MutationRecord[]>(handler => {
     const observer = new MutationObserver(handler as MutationCallback);
-    observer.observe(document.body, { attributes: true, attributeFilter: ['class'] });
+    observer.observe(document.documentElement, { attributes: true, attributeFilter: ['class'] });
     return () => observer.disconnect();
-  }).pipe(map(() => document.body.classList.contains('tum-apply-dark-mode')));
+  }).pipe(map(() => document.documentElement.classList.contains('tum-apply-dark-mode')));
   isDarkMode = toSignal(this.bodyClassChanges$, {
-    initialValue: document.body.classList.contains('tum-apply-dark-mode'),
+    initialValue: document.documentElement.classList.contains('tum-apply-dark-mode'),
   });
   translateService = inject(TranslateService);
   currentLanguage = toSignal(this.translateService.onLangChange.pipe(map(event => event.lang.toUpperCase())), {
@@ -125,12 +125,10 @@ export class HeaderComponent {
     void this.authFacadeService.logout();
   }
 
-  /*
   toggleColorScheme(): void {
     const className = 'tum-apply-dark-mode';
-    document.body.classList.toggle(className);
+    document.documentElement.classList.toggle(className);
   }
-  */
 
   toggleLanguage(language: string): void {
     if (this.languages.includes(language)) {
