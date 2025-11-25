8576d628508d3b12c2e8c850201d1cb6205dd053
Abinaya Anita Sivaguru <164672299+Abi107717@users.noreply.github.com>
2025-11-25 11:52:53 +0100

    `Development`: Implement interview slots calendar view (#1331)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Aniruddh Zaveri <92953467+az108@users.noreply.github.com>
    Co-authored-by: Bofan Zhu <56017927+Bofan-Zhu@users.noreply.github.com>
    Co-authored-by: Sehmuel Wagner <111253615+sachmii@users.noreply.github.com>
    Co-authored-by: Bofan Zhu <bofan.zhu@tum.de>
    Co-authored-by: Ramona Beinstingel <75392103+rabeatwork@users.noreply.github.com>
    Co-authored-by: aniruddhzaveri <aniruddh.zaveri@tum.de>

---
+++ src/main/webapp/app/interview/interview-process-detail/slots-section/month-navigation/month-navigation.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { Component, ViewEncapsulation, input, output } from '@angular/core';
+import { ButtonComponent } from 'app/shared/components/atoms/button/button.component';
+
+@Component({
+  selector: 'jhi-month-navigation',
+  imports: [CommonModule, ButtonComponent],
+  encapsulation: ViewEncapsulation.None,
+  templateUrl: './month-navigation.component.html',
+})
+export class MonthNavigationComponent {
+  currentMonth = input.required<string>();
+
+  previousMonth = output();
+  nextMonth = output();
+  addSlots = output();
+}
