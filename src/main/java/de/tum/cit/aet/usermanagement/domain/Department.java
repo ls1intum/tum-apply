a2cec360588a79201c49b7ed8c84ac36cb389773
Bofan Zhu <bofan.zhu@tum.de>
2025-11-26 14:48:51 +0100

    add new entities

---
+++ src/main/java/de/tum/cit/aet/usermanagement/domain/Department.java
@@ -0,0 +1,35 @@
+package de.tum.cit.aet.usermanagement.domain;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import de.tum.cit.aet.core.domain.AbstractAuditingEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "departments")
+@Getter
+@Setter
+public class Department extends AbstractAuditingEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @JdbcTypeCode(SqlTypes.VARCHAR)
+    @Column(name = "department_id", nullable = false)
+    private UUID departmentId;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "school_id", nullable = false)
+    private School school;
+
+    @OneToMany(mappedBy = "department", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<ResearchGroup> researchGroups = new HashSet<>();
+}
---
+++ src/main/java/de/tum/cit/aet/usermanagement/domain/School.java
@@ -0,0 +1,34 @@
+package de.tum.cit.aet.usermanagement.domain;
+
+import de.tum.cit.aet.core.domain.AbstractAuditingEntity;
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "schools")
+@Getter
+@Setter
+public class School extends AbstractAuditingEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @JdbcTypeCode(SqlTypes.VARCHAR)
+    @Column(name = "school_id", nullable = false)
+    private UUID schoolId;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "abbreviation", nullable = false)
+    private String abbreviation;
+
+    @OneToMany(mappedBy = "school", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Department> departments = new HashSet<>();
+}
