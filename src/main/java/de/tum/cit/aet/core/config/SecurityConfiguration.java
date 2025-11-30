419470c56adb2a8844da26c32f6a3cda369a81cd
Bofan Zhu <bofan.zhu@tum.de>
2025-11-30 13:45:12 +0100

    remove tests, run prettier

--- src/main/java/de/tum/cit/aet/core/config/SecurityConfiguration.java
+++ src/main/java/de/tum/cit/aet/core/config/SecurityConfiguration.java
@@ -135,7 +135,12 @@ public class SecurityConfiguration {
                     .requestMatchers("/api/export/job/**")
                     .permitAll()
                     // Public GET endpoints for schools
-                    .requestMatchers(org.springframework.http.HttpMethod.GET, "/api/schools", "/api/schools/with-departments", "/api/schools/*")
+                    .requestMatchers(
+                        org.springframework.http.HttpMethod.GET,
+                        "/api/schools",
+                        "/api/schools/with-departments",
+                        "/api/schools/*"
+                    )
                     .permitAll()
                     // Public GET endpoints for departments
                     .requestMatchers(org.springframework.http.HttpMethod.GET, "/api/departments", "/api/departments/*")
