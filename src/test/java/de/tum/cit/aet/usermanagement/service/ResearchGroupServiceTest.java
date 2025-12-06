2366e262f19e23bed7893abd254ddc4a0a1bc3c3
Sehmuel Wagner <111253615+sachmii@users.noreply.github.com>
2025-12-06 20:55:52 +0100

    fix server error

--- src/test/java/de/tum/cit/aet/usermanagement/service/ResearchGroupServiceTest.java
+++ src/test/java/de/tum/cit/aet/usermanagement/service/ResearchGroupServiceTest.java
@@ -3,6 +3,7 @@ package de.tum.cit.aet.usermanagement.service;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
