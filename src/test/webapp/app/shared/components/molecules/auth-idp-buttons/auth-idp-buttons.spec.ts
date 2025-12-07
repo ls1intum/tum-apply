26eee979d59a2a7bbee6fe1c52c3ca36f5fbfd71
Marc Fett <marc.fett@web.de>
2025-12-07 15:46:23 +0100

    change to test behavior

--- src/test/webapp/app/shared/components/molecules/auth-idp-buttons/auth-idp-buttons.spec.ts
+++ src/test/webapp/app/shared/components/molecules/auth-idp-buttons/auth-idp-buttons.spec.ts
@@ -53,21 +53,16 @@ describe('AuthIdpButtons', () => {
     const config = component.idpButtons();
 
     expect(component.onlyIcons()).toBe(false);
-    expect(config.direction).toBe('vertical');
-    expect(config.fullWidth).toBe(true);
+
     expect(config.buttons).toHaveLength(2);
 
     const [appleButton, googleButton] = config.buttons;
 
     expect(appleButton.label).toBe('Apple');
     expect(appleButton.icon).toBe('apple');
-    expect(appleButton.variant).toBe('outlined');
-    expect(appleButton.fullWidth).toBe(true);
 
     expect(googleButton.label).toBe('Google');
     expect(googleButton.icon).toBe('google');
-    expect(googleButton.variant).toBe('outlined');
-    expect(googleButton.fullWidth).toBe(true);
   });
 
   it('should configure buttons as icon-only and horizontal on small screens', () => {
@@ -87,19 +82,16 @@ describe('AuthIdpButtons', () => {
     const config = component.idpButtons();
 
     expect(component.onlyIcons()).toBe(true);
-    expect(config.direction).toBe('horizontal');
-    expect(config.fullWidth).toBe(true);
+
     expect(config.buttons).toHaveLength(2);
 
     const [appleButton, googleButton] = config.buttons;
 
     expect(appleButton.label).toBeUndefined();
     expect(appleButton.icon).toBe('apple');
-    expect(appleButton.variant).toBe('text');
 
     expect(googleButton.label).toBeUndefined();
     expect(googleButton.icon).toBe('google');
-    expect(googleButton.variant).toBe('text');
   });
 
   it('should call authFacadeService.loginWithProvider for Apple and Google with current origin', () => {
