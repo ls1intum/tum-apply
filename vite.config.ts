1097eb14579857dd16b94ba4a9847fa7c4d7737a
aniruddhzaveri <aniruddh.zaveri@tum.de>
2026-01-02 23:24:17 +0100

    update to angular 21

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 
 import angular from '@analogjs/vite-plugin-angular';
 import path from 'node:path';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig(({mode}) => ({
+export default defineConfig(({ mode }) => ({
   plugins: [
     angular(),
     tsconfigPaths({
@@ -23,7 +23,15 @@ export default defineConfig(({mode}) => ({
       reportsDirectory: 'build/test-results/lcov-report',
       provider: 'v8',
       all: true,
-      exclude: ['**/node_modules/**', '**/generated/**', '**/*.spec.ts', '**/*.test.ts', 'src/test/webapp/util/**', '**/*.html', '**/*.scss'],
+      exclude: [
+        '**/node_modules/**',
+        '**/generated/**',
+        '**/*.spec.ts',
+        '**/*.test.ts',
+        'src/test/webapp/util/**',
+        '**/*.html',
+        '**/*.scss',
+      ],
     },
   },
   define: {
