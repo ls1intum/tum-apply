7c8354c19bad119a800fb1e67ccc9ce5f9641e42
Celine Lahnor <celine.lahnor@tum.de>
2026-01-10 13:59:04 +0100

    codeStyle

--- src/main/webapp/app/shared/components/molecules/tab-view/tab-view.component.ts
+++ src/main/webapp/app/shared/components/molecules/tab-view/tab-view.component.ts
@@ -1,5 +1,7 @@
 import { Component, computed, effect, input, output, signal } from '@angular/core';
+
 import { CommonModule } from '@angular/common';
+
 import TranslateDirective from '../../../language/translate.directive';
 
 export interface TabItem {
