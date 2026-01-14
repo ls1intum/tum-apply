e032e9bc84d9d87db5cfd7422e91efebf4492f1b
Sehmuel Wagner <111253615+sachmii@users.noreply.github.com>
2026-01-14 17:22:03 +0100

    jsnon include non null

--- src/main/java/de/tum/cit/aet/notification/dto/ResearchGroupEmailContext.java
+++ src/main/java/de/tum/cit/aet/notification/dto/ResearchGroupEmailContext.java
@@ -1,9 +1,11 @@
 package de.tum.cit.aet.notification.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import de.tum.cit.aet.usermanagement.domain.ResearchGroup;
 import de.tum.cit.aet.usermanagement.domain.User;
 
 /**
  * Context object for research group emails that need both recipient and group data.
  */
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record ResearchGroupEmailContext(User user, ResearchGroup researchGroup) {}
