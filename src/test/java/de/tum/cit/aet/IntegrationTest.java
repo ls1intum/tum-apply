c6a2b8977692534ed4043b8f0658285471237f7e
Sehmuel Wagner <111253615+sachmii@users.noreply.github.com>
2026-01-20 08:36:52 +0100

    tests

--- src/test/java/de/tum/cit/aet/IntegrationTest.java
+++ src/test/java/de/tum/cit/aet/IntegrationTest.java
@@ -7,7 +7,9 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 /**
  * Base composite annotation for integration tests.
@@ -15,5 +17,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @SpringBootTest(classes = { TumApplyApp.class, JacksonConfiguration.class, AsyncSyncConfiguration.class })
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
 @EmbeddedSQL
 public @interface IntegrationTest {}
