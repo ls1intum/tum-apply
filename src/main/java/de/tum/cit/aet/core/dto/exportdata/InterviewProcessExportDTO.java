2d063395fef2aa04ff3c6ae1fe68076e3edf00c2
Sehmuel Wagner <111253615+sachmii@users.noreply.github.com>
2026-01-22 18:50:02 +0100

    fix include

--- src/main/java/de/tum/cit/aet/core/dto/exportdata/InterviewProcessExportDTO.java
+++ src/main/java/de/tum/cit/aet/core/dto/exportdata/InterviewProcessExportDTO.java
@@ -2,5 +2,5 @@ package de.tum.cit.aet.core.dto.exportdata;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
-@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public record InterviewProcessExportDTO(String jobTitle) {}
