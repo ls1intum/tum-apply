2c8b77dae5bc22425b407fd7ad3e1fd5aeb6b7e0
Abi107717 <a.abinaya.sivaguru@gmail.com>
2026-01-28 13:51:51 +0100

    consistency adjustments interview-processes-overview

--- src/main/webapp/app/interview/interview-processes-overview/upcoming-interviews-widget/upcoming-interview-card/upcoming-interview-card.component.ts
+++ src/main/webapp/app/interview/interview-processes-overview/upcoming-interviews-widget/upcoming-interview-card/upcoming-interview-card.component.ts
@@ -6,16 +6,16 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import dayjs from 'dayjs/esm';
 
 @Component({
-    selector: 'jhi-upcoming-interview-card',
-    standalone: true,
-    imports: [LocalizedDatePipe, RouterLink, FontAwesomeModule],
-    templateUrl: './upcoming-interview-card.component.html',
+  selector: 'jhi-upcoming-interview-card',
+  standalone: true,
+  imports: [LocalizedDatePipe, RouterLink, FontAwesomeModule],
+  templateUrl: './upcoming-interview-card.component.html',
 })
 export class UpcomingInterviewCardComponent {
-    interview = input.required<UpcomingInterviewDTO>();
+  interview = input.required<UpcomingInterviewDTO>();
 
-    formattedTimeRange = computed(() => {
-        const i = this.interview();
-        return `${dayjs(i.startDateTime).format('HH:mm')} - ${dayjs(i.endDateTime).format('HH:mm')}`;
-    });
+  formattedTimeRange = computed(() => {
+    const i = this.interview();
+    return `${dayjs(i.startDateTime).format('HH:mm')} - ${dayjs(i.endDateTime).format('HH:mm')}`;
+  });
 }
