c24fd34a3501cc3e087249014faf8df5ac893c7f
Kiara Copony <ge65min@mytum.de>
2026-02-06 22:34:34 +0100

    add limits back to PDF export

--- src/main/java/de/tum/cit/aet/core/service/PDFExportService.java
+++ src/main/java/de/tum/cit/aet/core/service/PDFExportService.java
@@ -107,24 +107,36 @@ public class PDFExportService {
         builder
             .startInfoSection(labels.get("bachelorInfo"))
             .addSectionData(labels.get("degreeName"), getValue(app.applicant().bachelorDegreeName()))
-            .addSectionData(labels.get("university"), getValue(app.applicant().bachelorUniversity()))
-            // .addSectionData(labels.get("upperGradeLimit"),
-            // getValue(app.applicant().bachelorGradeUpperLimit()))
-            // .addSectionData(labels.get("lowerGradeLimit"),
-            // getValue(app.applicant().bachelorGradeLowerLimit()))
-            .addSectionData(labels.get("grade"), getValue(app.applicant().bachelorGrade()));
+            .addSectionData(labels.get("university"), getValue(app.applicant().bachelorUniversity()));
+        if (
+            app.applicant().bachelorGradeUpperLimit() != null &&
+            !app.applicant().bachelorGradeUpperLimit().isBlank() &&
+            app.applicant().bachelorGradeLowerLimit() != null &&
+            !app.applicant().bachelorGradeLowerLimit().isBlank()
+        ) {
+            builder
+                .addSectionData(labels.get("upperGradeLimit"), getValue(app.applicant().bachelorGradeUpperLimit()))
+                .addSectionData(labels.get("lowerGradeLimit"), getValue(app.applicant().bachelorGradeLowerLimit()));
+        }
+        builder.addSectionData(labels.get("grade"), getValue(app.applicant().bachelorGrade()));
 
         // Master Section
         if (app.applicant().masterDegreeName() != null) {
             builder
                 .startInfoSection(labels.get("masterInfo"))
                 .addSectionData(labels.get("degreeName"), getValue(app.applicant().masterDegreeName()))
-                .addSectionData(labels.get("university"), getValue(app.applicant().masterUniversity()))
-                // .addSectionData(labels.get("upperGradeLimit"),
-                // getValue(app.applicant().masterGradeUpperLimit()))
-                // .addSectionData(labels.get("lowerGradeLimit"),
-                // getValue(app.applicant().masterGradeLowerLimit()))
-                .addSectionData(labels.get("grade"), getValue(app.applicant().masterGrade()));
+                .addSectionData(labels.get("university"), getValue(app.applicant().masterUniversity()));
+            if (
+                app.applicant().masterGradeUpperLimit() != null &&
+                !app.applicant().masterGradeUpperLimit().isBlank() &&
+                app.applicant().masterGradeLowerLimit() != null &&
+                !app.applicant().masterGradeLowerLimit().isBlank()
+            ) {
+                builder
+                    .addSectionData(labels.get("upperGradeLimit"), getValue(app.applicant().masterGradeUpperLimit()))
+                    .addSectionData(labels.get("lowerGradeLimit"), getValue(app.applicant().masterGradeLowerLimit()));
+            }
+            builder.addSectionData(labels.get("grade"), getValue(app.applicant().masterGrade()));
         }
 
         // Metadata
