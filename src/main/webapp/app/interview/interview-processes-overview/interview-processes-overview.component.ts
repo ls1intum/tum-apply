104d794cfc6ac53aa339bf79b9bc44dd96c634bf
Celine Lahnor <celine.lahnor@tum.de>
2026-03-11 14:20:07 +0100

    edited the default banner images to a more calm look

--- src/main/webapp/app/interview/interview-processes-overview/interview-processes-overview.component.ts
+++ src/main/webapp/app/interview/interview-processes-overview/interview-processes-overview.component.ts
@@ -92,10 +92,12 @@ export class InterviewProcessesOverviewComponent {
 
   private getExampleImageUrl(index: number): string {
     const headerImages = [
-      'https://images.unsplash.com/photo-1523240795612-9a054b0db644?w=800&q=80',
-      'https://images.unsplash.com/photo-1522202176988-66273c2fd55f?w=800&q=80',
-      'https://images.unsplash.com/photo-1582719471384-894fbb16e074?w=800&q=80',
-      'https://images.unsplash.com/photo-1543269865-cbf427effbad?w=800&q=80',
+      '/content/images/job-banner/job-banner1.png',
+      '/content/images/job-banner/job-banner2.png',
+      '/content/images/job-banner/job-banner3.png',
+      '/content/images/job-banner/job-banner4.png',
+      '/content/images/job-banner/job-banner5.png',
+      '/content/images/job-banner/job-banner6.png',
     ];
     return headerImages[index % headerImages.length];
   }
