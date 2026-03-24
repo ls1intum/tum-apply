0c3b948e78332b2a50e6a652e15d861d3ce7fcd6
aniruddhzaveri <aniruddh.zaveri@tum.de>
2026-03-24 21:00:24 +0100

    revert dialog component changes

--- src/main/webapp/app/shared/components/atoms/dialog/dialog.component.ts
+++ src/main/webapp/app/shared/components/atoms/dialog/dialog.component.ts
@@ -22,7 +22,6 @@ export class DialogComponent {
   closeOnEscape = input<boolean>(true);
   closable = input<boolean>(true);
   showHeader = input<boolean>(true);
-  useCustomHeader = input<boolean>(false);
   styleClass = input<string>('');
   contentStyleClass = input<string>('');
   contentStyle = input<Record<string, string>>({});
