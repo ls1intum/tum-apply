91f9707061c45248de12aa16c4494b3d95e1a802
aniruddhzaveri <aniruddh.zaveri@tum.de>
2026-03-27 21:48:39 +0100

    eslint

--- src/main/webapp/app/application/application-overview-for-applicant/application-overview-for-applicant.component.ts
+++ src/main/webapp/app/application/application-overview-for-applicant/application-overview-for-applicant.component.ts
@@ -144,9 +144,12 @@ export default class ApplicationOverviewForApplicantComponent {
 
       // Withdraw action - for SENT or IN_REVIEW applications
       if (
-        ([ApplicationOverviewDTOApplicationStateEnum.Sent, ApplicationOverviewDTOApplicationStateEnum.InReview] as ApplicationOverviewDTOApplicationStateEnum[]).includes(
-          application.applicationState as ApplicationOverviewDTOApplicationStateEnum,
-        )
+        (
+          [
+            ApplicationOverviewDTOApplicationStateEnum.Sent,
+            ApplicationOverviewDTOApplicationStateEnum.InReview,
+          ] as ApplicationOverviewDTOApplicationStateEnum[]
+        ).includes(application.applicationState as ApplicationOverviewDTOApplicationStateEnum)
       ) {
         items.push({
           label: 'button.withdraw',
