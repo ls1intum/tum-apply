506ded9fd2a9e1ca6b5a70b6f0d41d9b531ce0f6
Celine Lahnor <celine.lahnor@tum.de>
2026-03-29 12:35:23 +0200

    fix

--- src/main/webapp/app/shared/settings/email-settings/subject-area-subscription-selector.component.ts
+++ src/main/webapp/app/shared/settings/email-settings/subject-area-subscription-selector.component.ts
@@ -3,6 +3,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { TranslateModule } from '@ngx-translate/core';
 
 import { FilterChange, FilterMultiselect } from '../../components/atoms/filter-multiselect/filter-multiselect';
+
 import TranslateDirective from '../../language/translate.directive';
 import { SubjectArea } from './subject-area-subscriptions.store';
 
