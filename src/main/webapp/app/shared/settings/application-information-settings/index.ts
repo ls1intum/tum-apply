0c97c9d9d763e11e85c06e3569bfcc43c9a1ad4d
Celine Lahnor <celine.lahnor@tum.de>
2026-04-11 12:58:40 +0200

    renamed also in the codebase

---
+++ src/main/webapp/app/shared/settings/application-information-settings/index.ts
@@ -0,0 +1,3 @@
+export { ApplicationInformationSettingsComponent } from './application-information-settings.component';
+export type { ApplicationInformationData } from './application-information-settings.component';
+export { selectGender } from 'app/shared/constants/genders';
