7d20826425e88e11228fa47fcb272596a2e67713
aniruddhzaveri <aniruddh.zaveri@tum.de>
2026-04-12 22:30:44 +0200

    simplify searchWidth

--- src/main/webapp/app/shared/components/molecules/search-filter-sort-bar/search-filter-sort-bar.ts
+++ src/main/webapp/app/shared/components/molecules/search-filter-sort-bar/search-filter-sort-bar.ts
@@ -60,7 +60,7 @@ export class SearchFilterSortBar {
   selectedSortField = signal<string | undefined>(undefined);
   selectedSortDirection = signal<SortDirection>('DESC');
 
-  readonly searchFieldClass = computed(() => (this.searchWidth() ? `${this.searchWidth()} min-w-0` : 'max-w-[18rem] min-w-[12rem]'));
+  readonly searchFieldClass = computed(() => this.searchWidth() ?? 'max-w-[18rem]');
   readonly hasMobileActions = computed(() => this.filters().length || (this.sortableFields()?.length ?? 0));
   readonly hasActiveFilters = computed(() => Object.values(this.selectedFiltersById()).some(values => values.length));
 
