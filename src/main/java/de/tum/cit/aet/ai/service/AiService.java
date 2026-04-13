f464e3e09ad3978dc6ff9ed362d147b4e683ef06
Cathy <catherine.kalra@tum.de>
2026-04-13 09:53:31 +0200

    test

--- src/main/java/de/tum/cit/aet/ai/service/AiService.java
+++ src/main/java/de/tum/cit/aet/ai/service/AiService.java
@@ -68,6 +68,7 @@ public class AiService {
         this.currentUserService = currentUserService;
     }
 
+
     /**
      * Generates a job application draft using streaming for faster perceived response time.
      * Returns a Flux that emits content chunks as they are generated.
