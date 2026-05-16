77123ccf0c4b58f5032f5bf859432edf714fdd98
Lyynnnx <go98mip@mytum.de>
2026-05-16 19:12:18 +0200

    fix: codacy standards

--- src/main/webapp/app/core/auth/auth-facade.service.ts
+++ src/main/webapp/app/core/auth/auth-facade.service.ts
@@ -392,7 +392,7 @@ export class AuthFacadeService {
       // Check if passkey had an error because user refused to use passkey
       if (e instanceof DOMException && e.name === 'NotAllowedError') {
         console.log('Passkey prompt dismissed by user. Flow aborted cleanly.');
-        return undefined as any; 
+        return undefined as unknown as T; 
       }
       this.authOrchestrator.setError(errorMessage);
       throw e;
