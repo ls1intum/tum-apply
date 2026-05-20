830b45fc497764df2488b11168270e562bb2e5f7
Cathy <catherine.kalra@tum.de>
2026-05-20 14:28:32 +0200

    Refactor imprint page

--- src/main/webapp/app/shared/pages/imprint-page/imprint-page.component.ts
+++ src/main/webapp/app/shared/pages/imprint-page/imprint-page.component.ts
@@ -1,6 +1,11 @@
 import { Component, ViewEncapsulation } from '@angular/core';
-
 import TranslateDirective from '../../language/translate.directive';
+
+interface ImprintSection {
+  titleKey: string;
+  contentKey: string;
+  extraClasses?: string;
+}
 
 @Component({
   selector: 'jhi-imprint-page',
@@ -9,4 +14,14 @@ import TranslateDirective from '../../language/translate.directive';
   templateUrl: './imprint-page.component.html',
   encapsulation: ViewEncapsulation.None,
 })
-export class ImprintPageComponent {}
+export class ImprintPageComponent {
+  readonly sections: ImprintSection[] = [
+    { titleKey: 'imprint.publisher.headline', contentKey: 'imprint.publisher.contact', extraClasses: 'leading-8' },
+    { titleKey: 'imprint.authorized.headline', contentKey: 'imprint.authorized.text' },
+    { titleKey: 'imprint.vat.headline', contentKey: 'imprint.vat.text' },
+    { titleKey: 'imprint.responsibility.headline', contentKey: 'imprint.responsibility.text', extraClasses: 'leading-8' },
+    { titleKey: 'imprint.tou.headline', contentKey: 'imprint.tou.text' },
+    { titleKey: 'imprint.liability.headline', contentKey: 'imprint.liability.text' },
+    { titleKey: 'imprint.links.headline', contentKey: 'imprint.links.text' },
+  ];
+}
